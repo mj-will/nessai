4c16a1acd18b3a1c7f99953ca85e26ab828b37e4
mj-will <m.williams.4@research.gla.ac.uk>
2020-07-03 10:57:45 +0100

    Transition to nflows

--- flowproposal/flows.py
+++ flowproposal/flows.py
@@ -14,8 +14,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from nflows.flows import SimpleRealNVP, MaskedAutoregressiveFlow
 
-def setup_model(n_inputs=None,  n_conditional_inputs=None, augment_dim=None,
+
+def cpnest_setup_model(n_inputs=None,  n_conditional_inputs=None, augment_dim=None,
         n_neurons=128, n_layers=2, n_blocks=4, ftype='RealNVP', device='cpu',
         mask=None, prior='gaussian', **kwargs):
     """"
@@ -44,11 +46,7 @@ def setup_model(n_inputs=None,  n_conditional_inputs=None, augment_dim=None,
                 mask = 1 - mask
             model = FlowSequential(*blocks)
         else:
-            for n in range(n_blocks):
-                blocks += [AugmentedBlock(n_inputs, augment_dim, n_layers, n_neurons)]
-
-            model = AugmentedSequential(*blocks).to(device)
-            model.augment_dim = augment_dim
+            raise NotImplementedError
 
     elif ftype == 'realnvp_joint':
         if augment_dim is None:
@@ -62,11 +60,7 @@ def setup_model(n_inputs=None,  n_conditional_inputs=None, augment_dim=None,
                 mask = 1 - mask
             model = FlowSequential(*blocks)
         else:
-            for n in range(n_blocks):
-                blocks += [AugmentedBlock(n_inputs, augment_dim, n_layers, n_neurons)]
-
-            model = AugmentedSequential(*blocks).to(device)
-            model.augment_dim = augment_dim
+            raise NotImplementedError
 
     elif ftype == 'realnvp_mixer':
         if mask is None:
@@ -93,9 +87,55 @@ def setup_model(n_inputs=None,  n_conditional_inputs=None, augment_dim=None,
     else:
         raise ValueError('Unknown flow type, choose from RealNVP or MAF')
 
+
     model.to(device)
     model.device = device
+
     return model
+
+
+def setup_model(config):
+    """
+    Setup the flow form a configuration dictionary
+    """
+    kwargs = {}
+    flows = {'realnvp': SimpleRealNVP, 'maf': MaskedAutoregressiveFlow}
+    activations = {'relu': F.relu, 'tanh': F.tanh, 'swish': swish}
+
+    if 'kwargs' in config and (k:=config['kwargs']) is not None:
+        if 'activation' in k and isinstance(k['activation'], str):
+            try:
+                k['activation'] = activations[k['activatiob']]
+            except KeyError as e:
+                raise RuntimeError(f'Unknown activation function {e}')
+
+        kwargs.update(k)
+
+    if 'flow' in config and (c:=config['flow']) is not None:
+        model = c(config['n_inputs'], config['n_neurons'], config['n_blocks'],
+                config['n_layers'], **kwargs)
+    elif 'ftype' in config and (f:=config['ftype']) is not None:
+        if f.lower() not in flows:
+            raise RuntimeError((f'Unknown flow type: {f}. Choose from:'
+                f'{flows.keys()}'))
+
+        model = flows[f.lower()](config['n_inputs'], config['n_neurons'], config['n_blocks'],
+                config['n_layers'], **kwargs)
+
+    if 'device_tag' in config:
+        if isinstance(config['device_tag'], str):
+            device = torch.device(config['device_tag'])
+
+        try:
+            model.to(device)
+        except RuntimeError as e:
+            device = torch.device('cpu')
+            logger.warning(('Could not send the normailising flow to the specified'
+                f"device {config['device']} send to CPU instead. Error raised: {e}"))
+
+    model.device = device
+
+    return model, device
 
 def get_mask(in_features, out_features, in_flow_features, mask_type=None):
     """
@@ -660,134 +700,3 @@ class Swish(nn.Module):
         Forward pass of the function.
         '''
         return swish(x) # simply apply already implemented SiLU
-
-
-#class Transform(nn.Module):
-#    """
-#    Basic neural net to perform encoding or decoding
-#    """
-#    def __init__(self, in_dim, out_dim, n_neurons, n_layers=2):
-#        super(Transform, self).__init__()
-#
-#        self.linear1 = nn.Linear(in_dim, n_neurons)
-#        self.linear2 = nn.Linear(n_neurons, n_neurons)
-#        self.linear3 = nn.Linear(n_neurons, 2 * out_dim)
-#
-#        with torch.no_grad():
-#            self.linear3.weight.data.fill_(0.)
-#            self.linear3.bias.data.fill_(0.)
-#
-#    def forward(self, x):
-#        """
-#        Forward pass
-#        """
-#        x = self.linear1(x)
-#        x = swish(x)
-#        x = self.linear2(x)
-#        x = swish(x)
-#        x = self.linear3(x)
-#        x_s, x_m = torch.split(x, x.shape[-1] // 2, dim=1)
-#        x_s = torch.nn.functional.logsigmoid(x_s)
-#        x_s = torch.clamp(x_s, min=-2.5, max=2.5)
-#        return x_s, x_m
-
-
-class AugmentedBlock(nn.Module):
-    """
-    An implementation on augmented flow blocks
-
-    See: https://arxiv.org/abs/2002.07101
-    """
-    def __init__(self, x_dim, e_dim, n_layers=2, n_neurons=64):
-        """
-        Intialise the block
-        """
-        super(AugmentedBlock, self).__init__()
-        self.encoder = Transform(x_dim, e_dim, n_neurons)
-        self.decoder = Transform(e_dim, x_dim, n_neurons)
-
-    def forward(self, feature, augment, mode='forward'):
-        """
-        Forward or backwards pass
-        """
-        log_J = 0.
-        if mode == 'forward':
-            # encode e -> z | x
-            log_s, m = self.encoder(feature)
-            s = torch.exp(log_s)
-            z = s * augment + m
-            log_J += log_s.sum(-1, keepdim=True)
-            # decode x -> y | z
-            log_s, m = self.decoder(z)
-            s = torch.exp(log_s)
-            y = s * feature + m
-            log_J += log_s.sum(-1, keepdim=True)
-            return y, z, log_J
-        else:
-            # decode y -> x | z
-            log_s, m = self.decoder(augment)
-            s = torch.exp(-log_s)
-            x = s * (feature - m)
-            log_J -= log_s.sum(-1, keepdim=True)
-            # encode z -> e | z
-            log_s, m = self.encoder(x)
-            s = torch.exp(-log_s)
-            e = s * (augment - m)
-            log_J -= log_s.sum(-1, keepdim=True)
-            return x, e, log_J
-
-
-class AugmentedSequential(nn.Sequential):
-    """
-    A sequential container for augmented flows
-    """
-    def forward(self, feature, augment, mode='forward'):
-        """
-        Forward or backward pass through the flows
-        """
-        log_dets = torch.zeros(feature.size(0), 1, device=feature.device)
-        if mode == 'forward':
-            for module in self._modules.values():
-                feature, augment, log_J = module(feature, augment, mode)
-                log_dets += log_J
-        else:
-            for module in reversed(self._modules.values()):
-                feature, augment, log_J = module(feature, augment, mode)
-                log_dets += log_J
-
-        return feature, augment, log_dets
-
-    def log_N(self, x):
-        """
-        Calculate of the log probability of an N-dimensional gaussian
-        """
-        return (-0.5 * x.pow(2) - 0.5 * np.log(2 * np.pi)).sum(
-            -1, keepdim=True)
-
-    def log_p_xe(self, feature, augment):
-        """
-        Calculate the joint log probability p(x, e)
-        """
-        # get transformed features
-        y, z, log_J = self(feature, augment)
-        # y & z should be gaussian
-        y_prob = self.log_N(y)
-        z_prob = self.log_N(z)
-        return (y_prob + z_prob + log_J).sum(-1, keepdim=True)
-
-    def log_p_x(self, feature, e_dim, K=1000):
-        """
-        Calculate the lower bound of the marginalised log probability p(x)
-        """
-        log_p_x = torch.zeros(feature.size(0), 1, device=feature.device)
-        # get log p(x, e)
-        for i,f in enumerate(feature):
-            e = torch.Tensor(K, e_dim).normal_().to(feature.device)
-            log_q = self.log_N(e)
-            # need to pass the same feature K times (for each e)
-            f_repeated = f * torch.ones(K, f.size(0), device=feature.device)
-            log_p_xe = self.log_p_xe(f_repeated, e)
-            # compute sum of ratio
-            lpx = -np.log(K) + torch.logsumexp(log_p_xe - log_q, (0))
-            log_p_x[i] = lpx
-        return log_p_x
--- flowproposal/proposal.py
+++ flowproposal/proposal.py
@@ -3,7 +3,7 @@ import logging
 import numpy as np
 import torch
 
-from .flowmodel import FlowModel, update_config
+from .flowmodel import FlowModel, CPnestFlowModel, update_config
 from .livepoint import live_points_to_array, numpy_array_to_live_points, get_dtype
 from .plot import plot_live_points
 
@@ -14,12 +14,12 @@ class Proposal:
     def __init__(self, model):
         self.model = model
         self.populated = True
-        self.intialised = False
+        self.initialised = False
         self.training_count = 0
 
     def initialise(self):
         """Initialise"""
-        self.intialised = True
+        self.initialised = True
 
     def draw(self, old_param):
         return None
@@ -29,7 +29,6 @@ class Proposal:
 
     def __getstate__(self):
         state = self.__dict__.copy()
-        state['initialised'] = False
         del state['model']
         return state
 
@@ -88,7 +87,7 @@ class RejectionProposal(Proposal):
         return new_sample
 
 
-class FlowProposal(Proposal):
+class CPnestFlowProposal(Proposal):
     """
     Object that handles training and proposal points
     """
@@ -100,7 +99,7 @@ class FlowProposal(Proposal):
         """
         Initialise
         """
-        super(FlowProposal, self).__init__(model)
+        super(CPnestFlowProposal, self).__init__(model)
         logger.debug('Initialising FlowProposal')
 
         self.flow = None
@@ -175,8 +174,9 @@ class FlowProposal(Proposal):
             os.makedirs(self.output, exist_ok=True)
         self.set_rescaling()
         self.flow_config['model_config']['n_inputs'] = self.rescaled_dims
-        self.flow = FlowModel(config=self.flow_config, output=self.output)
+        self.flow = CPnestFlowModel(config=self.flow_config, output=self.output)
         self.flow.initialise()
+        self.populated = False
         self.initialised = True
 
     def set_rescaling(self):
@@ -436,3 +436,342 @@ class FlowProposal(Proposal):
             logger.debug('Proposal pool is empty')
         # make live point and return
         return new_sample
+
+
+class FlowProposal(Proposal):
+    """
+    Object that handles training and proposal points
+    """
+
+    def __init__(self, model, flow_config=None, output='./', poolsize=10000,
+            rescale_parameters=False, latent_prior='truncated_gaussian', fuzz=1.0,
+            keep_samples=False, exact_poolsize=True, plot=True, fixed_radius=False,
+            **kwargs):
+        """
+        Initialise
+        """
+        super(FlowProposal, self).__init__(model)
+        logger.debug('Initialising FlowProposal')
+
+        self.flow = None
+        self.initialised = False
+        self.populated = False
+        self.indices = []
+        self.training_count = 0
+        self.populated_count = 0
+        self.names = []
+        self.x = None
+        self.z = None
+        self.rescaled_names = []
+
+        self.output = output
+        self.poolsize = poolsize
+        self.fuzz = fuzz
+        self.latent_prior = latent_prior
+        self.rescale_parameters = rescale_parameters
+        self.keep_samples = keep_samples
+        self.exact_poolsize = exact_poolsize
+
+        self.flow_config = update_config(flow_config)
+
+        if self.latent_prior == 'truncated_gaussian':
+            from .utils import draw_truncated_gaussian
+            self.draw_latent_prior = draw_truncated_gaussian
+        if self.latent_prior == 'gaussian':
+            logger.warning('Using a gaussian latent prior WITHOUT truncation')
+            from .utils import draw_gaussian
+            self.draw_latent_prior = draw_gaussian
+        elif self.latent_prior == 'uniform':
+            from .utils import draw_random_nsphere
+            self.draw_latent_prior = draw_random_nsphere
+
+        if fixed_radius:
+            try:
+                self.fixed_radius = float(fixed_radius)
+            except ValueError:
+                logger.error('Fixed radius enabled but could not be converted to a float. Setting fixed_radius=False')
+                self.fixed_radius = False
+        else:
+            self.fixed_radius = False
+
+    @property
+    def dims(self):
+        """Return the number of dimensions"""
+        return len(self.names)
+
+    @property
+    def rescaled_dims(self):
+        """Return the number of rescaled dimensions"""
+        return len(self.rescaled_names)
+
+    @property
+    def x_dtype(self):
+        """Return the dtype for the x space"""
+        return get_dtype(self.names, 'f8')
+
+    @property
+    def x_prime_dtype(self):
+        """Return the dtype for the x prime space"""
+        return get_dtype(self.rescaled_names, 'f8')
+
+    def initialise(self):
+        """
+        Initialise the proposal class
+        """
+        if not os.path.exists(self.output):
+            os.makedirs(self.output, exist_ok=True)
+        self.set_rescaling()
+        self.flow_config['model_config']['n_inputs'] = self.rescaled_dims
+        self.flow = FlowModel(config=self.flow_config, output=self.output)
+        self.flow.initialise()
+        self.populated = False
+        self.initialised = True
+
+    def set_rescaling(self):
+        """
+        Set function and parameter names for rescaling
+        """
+        self.names = self.model.names.copy()
+        self.rescaled_names = self.names.copy()
+        # if rescale, update names
+        if self.rescale_parameters:
+            # if rescale is a list, there are the parameters to rescale
+            # else all parameters are rescale
+            if not isinstance(self.rescale_parameters, list):
+                self.rescale_parameters = self.names
+            for i, rn in enumerate(self.rescaled_names):
+                if rn in self.rescale_parameters:
+                    self.rescaled_names[i] += '_prime'
+            self.rescale = self._rescale_with_bounds
+            self.inverse_rescale = self._inverse_rescale_with_bounds
+
+        logger.info(f'x space parameters: {self.names}')
+        logger.info(f'parameters to rescale {self.rescale_parameters}')
+        logger.info(f'x prime space parameters: {self.rescaled_names}')
+
+    def _rescale_with_bounds(self, x):
+        """
+        Rescale the inputs to [-1, 1] using the bounds as the min and max
+        """
+        x_prime = np.zeros([x.size], dtype=self.x_prime_dtype)
+        log_J = 0.
+        for n, rn in zip(self.names, self.rescaled_names):
+            if n in self.rescale_parameters:
+                x_prime[rn] = 2 * ((x[n] - self.model.bounds[n][0]) \
+                    / (self.model.bounds[n][1] - self.model.bounds[n][0])) - 1
+
+                log_J += np.log(2) - np.log(self.model.bounds[n][1] \
+                        - self.model.bounds[n][0])
+            else:
+                x_prime[rn] = x[n]
+        x_prime['logP'] = x['logP']
+        x_prime['logL'] = x['logL']
+        return x_prime, log_J
+
+    def _inverse_rescale_with_bounds(self, x_prime):
+        """
+        Rescale the inputs from the prime space to the phyiscal space
+        using the model bounds
+        """
+        x = np.zeros([x_prime.size], dtype=self.x_dtype)
+        log_J = 0.
+        for n, rn in zip(self.names, self.rescaled_names):
+            if n in self.rescale_parameters:
+                x[n] = (self.model.bounds[n][1] - self.model.bounds[n][0]) \
+                        * ((x_prime[rn] + 1) / 2) + self.model.bounds[n][0]
+                log_J += np.log(self.model.bounds[n][1] - self.model.bounds[n][0]) \
+                        - np.log(2)
+            else:
+                x[n] = x_prime[rn]
+        x['logP'] = x_prime['logP']
+        x['logL'] = x_prime['logL']
+        return x, log_J
+
+    def rescale(self, x):
+        """
+        Rescale from the phyisical space to the primed physical
+        space
+        """
+        log_J = 0.
+        return x, log_J
+
+    def inverse_rescale(self, x_prime):
+        """
+        Rescale from the primed phyisical space to the original physical
+        space
+        """
+        log_J = 0.
+        return x_prime, log_J
+
+    def train(self, x, plot=True):
+        """
+        Train the normalising flow given the live points
+        """
+        block_output = self.output + f'/training/block_{self.training_count}/'
+        if not os.path.exists(block_output):
+            os.makedirs(block_output, exist_ok=True)
+
+        if plot:
+            plot_live_points(x, filename=block_output + 'x_samples.png')
+
+        x_prime, log_J = self.rescale(x)
+
+        if self.rescale_parameters and plot:
+            plot_live_points(x_prime, filename=block_output + 'x_prime_samples.png')
+        # Convert to numpy array for training and remove likelihoods and priors
+        # Since the names of parameters may have changes, pull names from flows
+        x_prime = live_points_to_array(x_prime, self.rescaled_names)
+        self.flow.train(x_prime, output=block_output, plot=plot)
+
+        if plot:
+            z = np.random.randn(5000, self.rescaled_dims)
+            x_prime, log_J = self.backward_pass(z, rescale=False)
+            plot_live_points(x_prime, filename=block_output + 'x_prime_generated.png')
+            x, log_J = self.inverse_rescale(x_prime)
+            if self.rescale_parameters:
+                plot_live_points(x, filename=block_output + 'x_generated.png')
+
+        self.populated = False
+        self.training_count += 1
+
+    def reset_model_weights(self):
+        """
+        Reset the flows weights
+        """
+        self.flow.reset_model()
+
+    def forward_pass(self, x, rescale=True):
+        """Pass a vector of points through the model"""
+        log_J = 0
+        if rescale:
+            x, log_J_rescale = self.rescale(x)
+            log_J += log_J_rescale
+        x = live_points_to_array(x, names=self.rescaled_names)
+        z, log_prob = self.flow.forward_and_log_prob(x)
+        return z, log_prob + log_J
+
+    def backward_pass(self, z, rescale=True):
+        """A backwards pass from the model (latent -> real)"""
+        # Compute the log probability
+        x, log_prob = self.flow.sample_and_log_prob(z=z)
+        x = numpy_array_to_live_points(x.astype('f8'), self.rescaled_names)
+        # Apply rescaling in rescale=True
+        if rescale:
+            x, log_J = self.inverse_rescale(x)
+            # Include Jacobian for the rescaling
+            log_prob -= log_J
+        return x, log_prob
+
+    def radius(self, z):
+        """Calculate the radius of a latent_point"""
+        return np.squeeze(np.sqrt(np.sum(z ** 2., axis=-1)))
+
+    def log_prior(self, x):
+        """
+        Compute the prior probability
+        """
+        return self.model.log_prior(x)
+
+    def compute_weights(self, x, log_q):
+        """
+        Compute the weight for a given set of samples
+        """
+        log_p = self.log_prior(x)
+        x['logP'] = log_p
+        x['logL'] = log_q
+        log_w = log_p - log_q
+        log_w -= np.max(log_w)
+        return log_w
+
+    def populate(self, worst_point, N=10000, plot=True):
+        """Populate a pool of latent points"""
+        if self.fixed_radius:
+            r = self.fixed_radius
+        else:
+            worst_z, _ = self.forward_pass(worst_point, rescale=True)
+            r = self.radius(worst_z)
+        logger.debug(f'Populating proposal with lantent radius: {r:.5}')
+        warn = True
+        if not self.keep_samples or not self.indices:
+            self.x = np.array([], dtype=self.x_dtype)
+            self.z = np.empty([0, self.dims])
+        while len(self.x) < N:
+            while True:
+                z = self.draw_latent_prior(self.dims, r, 10 * N, fuzz=self.fuzz)
+                if z.size:
+                    break
+
+            x, log_q = self.backward_pass(z, rescale=True)
+            # rescale given priors used initially, need for priors
+            log_w = self.compute_weights(x, log_q)
+            log_u = np.log(np.random.rand(x.shape[0]))
+            indices = np.where((log_w - log_u) >= 0)[0]
+            if not len(indices):
+                logger.error('Rejection sampling produced zero samples!')
+                raise RuntimeError('Rejection sampling produced zero samples!')
+            if len(indices) / N < 0.01:
+                if warn:
+                    logger.warning('Rejection sampling accepted less than 1 percent of samples!')
+                    warn = False
+            else:
+                # array of indices to take random draws from
+                self.x = np.concatenate([self.x, x[indices]], axis=0)
+                self.z = np.concatenate([self.z, z[indices]], axis=0)
+
+        if self.exact_poolsize:
+            self.x = self.x[:N]
+            self.z = self.z[:N]
+
+        if plot:
+            plot_live_points(self.x,
+                    filename=f'{self.output}/pool_{self.populated_count}.png')
+
+        self.samples = self.x[self.model.names + ['logP', 'logL']]
+
+        self.indices = np.random.permutation(self.samples.size).tolist()
+        self.populated = True
+        logger.debug(f'Proposal populated with {len(self.indices)} samples')
+
+    def evaluate_likelihoods(self):
+        """
+        Evaluate the likelihoods for the pool of live points
+        """
+        pass
+
+    def draw(self, worst_point):
+        """
+        Draw a replacement point
+        """
+        if not self.populated:
+            while not self.populated:
+                self.populate(worst_point, N=self.poolsize)
+            self.populated_count += 1
+        # new sample is drawn randomly from proposed points
+        # popping from right end is faster
+        index = self.indices.pop()
+        new_sample = self.samples[index]
+        if not self.indices:
+            self.populated = False
+            logger.debug('Proposal pool is empty')
+        # make live point and return
+        return new_sample
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state['initialised'] = False
+        state['weights_file'] = state['flow'].weights_file
+        # user provides model and config for resume
+        # flow can be reconstructed from resume
+        del state['model']
+        del state['flow_config']
+        del state['flow']
+        del state['rescale']
+        del state['inverse_rescale']
+        del state['x']
+        del state['z']
+        del state['indices']
+        del state['samples']
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__ = state
