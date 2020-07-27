444e6435dd713204ad0d2ac39390e0f0c40a8735
mj-will <m.williams.4@research.gla.ac.uk>
2020-07-27 13:35:15 +0100

    Update setup and readme

--- setup.py
+++ setup.py
@@ -7,10 +7,21 @@ setuptools.setup(
     name="flowproposal",
     version="0.0.1",
     author="Michael J. Williams",
+    url="https://gilsay.physics.gla.ac.uk/gitlab/michael.williams/flowproposal",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'pandas',
+        'tqdm',
+        'scipy',
+        'nflows'
+        ],
 )
