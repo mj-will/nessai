3fdfbfd83bbbe10a9ab24ed825533fddc4d9990e
mj-will <m.williams.4@research.gla.ac.uk>
2020-08-12 15:21:33 +0100

    Add test for logsubexp

---
+++ tests/test_posterior.py
@@ -0,0 +1,12 @@
+import pytest
+from flowproposal.posterior import (
+        logsubexp
+        )
+
+
+def test_logsubexp_negative():
+    """
+    Test behaviour of logsubexp for x < y
+    """
+    with pytest.raises(Exception):
+        logsubexp(1, 2)
