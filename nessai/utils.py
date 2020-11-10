377ac83787092a2a3f43fa5438c81e6251f34f28
mj-will <m.williams.4@research.gla.ac.uk>
2020-11-10 09:28:19 +0000

    Tweak edge detection and move auto_bins

--- nessai/utils.py
+++ nessai/utils.py
@@ -283,7 +283,7 @@ def replace_in_list(target_list, targets, replacements):
             replacements = list(replacements)
 
     if not all([t in target_list for t in targets]):
-        raise ValueError(f'Target(s) not in target list: {targets}')
+        raise ValueError(f'Targets {targets} not in list: {target_list}')
 
     for t, r in zip(targets, replacements):
         i = target_list.index(t)
@@ -398,9 +398,11 @@ def detect_edge(x, percent=0.1, cutoff=0.5, nbins='auto',
         If not None this skips the process and just returns the value of test.
         This is used to verify the inversion in all possible scenarios.
     """
-    bounds = ['lower', 'upper']
     if test is not None:
         return test
+    bounds = ['lower', 'upper']
+    if nbins == 'auto':
+        nbins = auto_bins(x)
     hist, bins = np.histogram(x, bins=nbins, density=True)
     n = max(int(len(bins) * percent), 1)
     bounds_fraction = \
@@ -628,3 +630,77 @@ def configure_threads(max_threads=None, pytorch_threads=None, n_pool=None):
         logger.debug(
             f'Setting maximum number of PyTorch threads to {pytorch_threads}')
         torch.set_num_threads(pytorch_threads)
+
+
+def _hist_bin_fd(x):
+    """
+    The Freedman-Diaconis histogram bin estimator.
+
+    See original Numpy implementation.
+
+    Parameters
+    ----------
+    x : array_like
+        Input data that is to be histogrammed, trimmed to range. May not
+        be empty.
+    Returns
+    -------
+    h : An estimate of the optimal bin width for the given data.
+    """
+    iqr = np.subtract(*np.percentile(x, [75, 25]))
+    return 2.0 * iqr * x.size ** (-1.0 / 3.0)
+
+
+def _hist_bin_sturges(x):
+    """
+    Sturges histogram bin estimator.
+
+    See original Numpy implementation.
+
+    Parameters
+    ----------
+    x : array_like
+        Input data that is to be histogrammed, trimmed to range. May not
+        be empty.
+    Returns
+    -------
+    h : An estimate of the optimal bin width for the given data.
+    """
+    return np.ptp(x) / (np.log2(x.size) + 1.0)
+
+
+def auto_bins(x, max_bins=50):
+    """
+    Compute the number bins for a histogram using numpy.histogram_bin_edges
+    but enforece a maximum number of bins.
+
+    Parameters
+    ----------
+    array : array_like
+        Input data
+    bins : int or sequence of scalars or str, optional
+        Method for determining number of bins, see numpy documentation
+    max_bins : int, optional (1000)
+        Maximum number of bins
+
+    Returns
+    -------
+    int
+        Number of bins
+    """
+    x = np.asarray(x)
+    fd_bw = _hist_bin_fd(x)
+    sturges_bw = _hist_bin_sturges(x)
+    if fd_bw:
+        bw = min(fd_bw, sturges_bw)
+    else:
+        bw = sturges_bw
+
+    if bw:
+        n_bins = int(np.ceil(np.ptp(x)) / bw)
+    else:
+        n_bins = 1
+
+    nbins = min(n_bins, max_bins)
+    assert isinstance(nbins, int)
+    return nbins
