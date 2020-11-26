71fe03573ddc955d2b6317907068ef15639e75c3
mj-will <m.williams.4@research.gla.ac.uk>
2020-11-26 13:08:07 +0000

    Add logit conversion and distance converter

--- nessai/gw/utils.py
+++ nessai/gw/utils.py
@@ -1,6 +1,13 @@
 import logging
 import numpy as np
-import scipy.stats as stats
+from scipy import stats, interpolate
+
+from ..utils import (
+    logit,
+    sigmoid,
+    rescale_zero_to_one,
+    inverse_rescale_zero_to_one
+)
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +17,13 @@ try:
 except ImportError:
     logger.debug(
         'Could not import LAL functions, running with reduced functionality')
+
+try:
+    from astropy import cosmology as cosmo
+    import astropy.units as u
+except ImportError:
+    logger.debug(
+        'Could not import astropy, running with reduced functionality')
 
 
 def angle_to_cartesian(alpha, r=None, scale=1.0):
@@ -208,9 +222,15 @@ def cartesian_to_zero_one(x, y):
     return theta, radius, -np.log(radius)
 
 
-def _spin_jacobian(sx, sy, sz):
-    return -0.5 * np.log(1 + (sz ** 2 / (sx ** 2 + sy ** 2))) \
-            + np.log(sx ** 2 + sy ** 2 + sz ** 2)
+def _spin_jacobian_to_precessing(sx, sy, sz):
+    """Log Jacobian for transformation from spin vector to angles"""
+    return - 0.5 * np.log(sx ** 2 + sy ** 2) \
+           - 0.5 * np.log(sx ** 2 + sy ** 2 + sz ** 2)
+
+
+def _spin_jacobian_from_precessing(a, theta):
+    """Log jacobian for tranformation from precessing to spin vector"""
+    return 2 * np.log(a) + np.log(np.sin(theta))
 
 
 @np.vectorize
@@ -225,8 +245,8 @@ def transform_from_precessing_parameters(theta_jn, phi_jl, theta_1, theta_2,
                 theta_jn, phi_jl, theta_1, theta_2, phi_12, a_1, a_2, m1, m2,
                 f_ref, phase)
 
-    log_J = _spin_jacobian(s1x, s1y, s1z)
-    log_J += _spin_jacobian(s2x, s2y, s2z)
+    log_J = _spin_jacobian_from_precessing(a_1, theta_1)
+    log_J += _spin_jacobian_from_precessing(a_2, theta_2)
     return iota, s1x, s1y, s1z, s2x, s2y, s2z, log_J
 
 
@@ -238,6 +258,55 @@ def transform_to_precessing_parameters(iota, s1x, s1y, s1z, s2x, s2y, s2z,
         lalsim.SimInspiralTransformPrecessingWvf2PE(
             iota, s1x, s1y, s1z, s2x, s2y, s2z, m1, m2, f_ref, phase)
 
-    log_J = -_spin_jacobian(s1x, s1y, s1z)
-    log_J -= _spin_jacobian(s2x, s2y, s2z)
+    log_J = _spin_jacobian_to_precessing(s1x, s1y, s1z)
+    log_J += _spin_jacobian_to_precessing(s2x, s2y, s2z)
     return theta_jn, phi_jl, theta_1, theta_2, phi_12, a_1, a_2, log_J
+
+
+def rescale_and_logit(x, xmin, xmax):
+    x, log_J = rescale_zero_to_one(x, xmin, xmax)
+    x, log_J_logit = logit(x)
+    return x, log_J + log_J_logit
+
+
+def rescale_and_sigmoid(x, xmin, xmax):
+    x, log_J_sig = sigmoid(x)
+    x, log_J = inverse_rescale_zero_to_one(x, xmin, xmax)
+    return x, log_J + log_J_sig
+
+
+class DistanceConverter:
+    """
+    Object to convert comoving distance to luminosity distance and vice
+    versa
+    """
+    def __init__(self, dl_min=100, dl_max=3000, units=u.Mpc,
+                 cosmology='Planck15'):
+        self.dl_min = dl_min
+        self.dl_max = dl_max
+        self.units = units
+        # TODO: this needs to update with bilby
+        self.cosmology = cosmo.Planck15
+
+        self.dc_min = self.cosmology.comoving_distance(cosmo.z_at_value(
+            self.cosmology.luminosity_distance, dl_min * self.units)).value
+        self.dc_max = self.cosmology.comoving_distance(cosmo.z_at_value(
+            self.cosmology.luminosity_distance, dl_max * self.units)).value
+
+        logger.debug('Making distance look up table')
+
+        dc_array = np.linspace(self.dc_min, self.dc_max, 500)
+        dl_array = self.cosmology.luminosity_distance(
+            [cosmo.z_at_value(self.cosmology.comoving_distance, d * self.units)
+                for d in dc_array]).value
+
+        self.interp_dc2dl = interpolate.splrep(dc_array, dl_array)
+        self.interp_dl2dc = interpolate.splrep(dl_array, dc_array)
+
+    def comoving_distance_to_luminosity_distance(self, dc):
+        """Convert comoving distance to luminosity distance"""
+        return interpolate.splev(dc, self.interp_dc2dl, ext=3)
+
+    def luminosity_distance_to_comoving_distance(self, dl):
+        """Convert luminosity distance to comoving distance"""
+        return interpolate.splev(dl, self.interp_dl2dc, ext=3)
