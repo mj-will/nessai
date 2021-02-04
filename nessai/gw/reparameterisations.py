dcfdbc03765e0afa1334f59236aab38a26fc6ed6
mj-will <m.williams.4@research.gla.ac.uk>
2021-02-04 16:42:00 +0000

    Change distance reparameterisation to use pre-rescaling

--- nessai/gw/reparameterisations.py
+++ nessai/gw/reparameterisations.py
@@ -1,8 +1,6 @@
 
 import logging
 
-import numpy as np
-
 from ..reparameterisations import (
     default_reparameterisations,
     RescaleToBounds,
@@ -35,6 +33,35 @@ def get_gw_reparameterisation(reparameterisation):
 
 
 class DistanceReparameterisation(RescaleToBounds):
+    """Reparameterisation for distance.
+
+    If the prior is specified and is one of the known priors then a rescaling
+    is applied such that the resulting parameter has a uniform prior. If the
+    prior is not specified, then the distance is rescaled an inversion is
+    allowed on only the upper bound.
+
+    Known priors
+    ------------
+    * Power-law: requires specifying the power. See converter kwargs.
+    * Uniform-comoving-volume: uses a lookup table to convert to co-moving
+    distance.
+
+    Parameters
+    ----------
+    parameters : str
+        Name of distance parameter to rescale.
+    prior : {'power-law', 'uniform-comoving-volume'}, optional
+        Prior used for the distance parameter
+    prior_bounds : tuple
+        Tuple of lower and upper bounds on the prior
+    conveter_kwargs : dict, optional
+        Keyword arguments parsed to converter object that converts the distance
+        to a parameter with a uniform prior.
+    allowed_bounds : list, optional
+        List of the allowed bounds for inversion
+    kwargs :
+        Additional kwargs are parsed to the parent class.
+    """
     def __init__(self, parameters=None, allowed_bounds=['upper'],
                  allow_both=False, converter_kwargs=None, prior=None,
                  prior_bounds=None, **kwargs):
@@ -54,94 +81,28 @@ class DistanceReparameterisation(RescaleToBounds):
             d_max=prior_bounds[parameters[0]][1],
             **converter_kwargs)
 
+        pre_rescaling = (self.distance_converter.to_uniform_parameter,
+                         self.distance_converter.from_uniform_parameter)
+
         super().__init__(parameters=parameters, prior=prior,
-                         prior_bounds=prior_bounds, **kwargs)
+                         prior_bounds=prior_bounds,
+                         pre_rescaling=pre_rescaling, **kwargs)
 
         if self.distance_converter.has_conversion:
             self._prime_prior = log_uniform_prior
             self.has_prime_prior = True
-            self.detect_edge_prime = True
-            self.requires_prime_prior = True
+            if not self.distance_converter.has_jacobian:
+                logger.debug('Distance converter does not have Jacobian, '
+                             'require prime prior')
+                self.requires_prime_prior = True
+            self.update_prime_prior_bounds()
         else:
             self.has_prime_prior = False
-            self.detect_edge_prime = False
-
-        self.orig_prior_bounds = self.prior_bounds.copy()
-        self.prior_bounds = \
-            {self.parameters[0]: self.convert_to_uniform_parameter(
-                self.prior_bounds[self.parameters[0]])}
 
         self.detect_edges_kwargs['allowed_bounds'] = allowed_bounds
         self.detect_edges_kwargs['allow_both'] = allow_both
         self.detect_edges_kwargs['x_range'] = \
             self.prior_bounds[self.parameters[0]]
-
-    def convert_to_uniform_parameter(self, d):
-        """
-        Convert to uniform distance parameter dc3
-        """
-        return self.distance_converter.to_uniform_parameter(d)
-
-    def convert_from_uniform_parameter(self, d):
-        """
-        Convert from uniform distance parameter to luminosity_distance
-        """
-        return self.distance_converter.from_uniform_parameter(d)
-
-    def reparameterise(self, x, x_prime, log_j, compute_radius=False,
-                       **kwargs):
-        """
-        Rescale inputs to the prime space
-
-        Parameters
-        ----------
-        x, x_prime :  array_like
-            Arrays of samples in the physical and prime space
-        log_j : array_like
-            Array of values of log-Jacboian
-        compute_radius : bool, optional
-            If true force duplicate for inversion
-        kwargs :
-            Parsed to inversion function
-        """
-        for p, pp in zip(self.parameters, self.prime_parameters):
-            x_prime[pp] = self.convert_to_uniform_parameter(x[p])
-            if p in self.boundary_inversion:
-                x, x_prime, log_j = self._apply_inversion(
-                        x, x_prime, log_j, p, pp, compute_radius,
-                        **kwargs)
-            else:
-                x_prime[pp], lj = \
-                    self._rescale_to_bounds(x_prime[pp] - self.offsets[p], p)
-                log_j += lj
-        return x, x_prime, log_j
-
-    def inverse_reparameterise(self, x, x_prime, log_j, **kwargs):
-        """Map inputs to the physical space from the prime space"""
-        for p, pp in zip(reversed(self.parameters),
-                         reversed(self.prime_parameters)):
-            if p in self.boundary_inversion:
-                x, x_prime, log_j = self._reverse_inversion(
-                    x, x_prime, log_j, p, pp, **kwargs)
-            else:
-                x[p], lj = self._inverse_rescale_to_bounds(x_prime[pp], p)
-                x[p] += self.offsets[p]
-                log_j += lj
-            x[p] = self.convert_from_uniform_parameter(x[p])
-        return x, x_prime, log_j
-
-    def update_bounds(self, x):
-        """Update the bounds used for the reparameterisation"""
-        if self._update_bounds or self.bounds is None:
-            up = self.convert_to_uniform_parameter(x[self.parameters[0]])
-            self.bounds = \
-                {p: [np.min(up - self.offsets[p]),
-                     np.max(up - self.offsets[p])]
-                 for p in self.parameters}
-            logger.debug(f'New bounds: {self.bounds}')
-            self.update_prime_prior_bounds()
-        else:
-            logger.debug('Update bounds not enabled')
 
 
 default_gw = {
@@ -153,7 +114,8 @@ default_gw = {
     'sky-az-zen': (AnglePair, {'convention': 'az-zen'}),
     'mass_ratio': (RescaleToBounds, {'detect_edges': True,
                                      'boundary_inversion': True,
-                                     'inversion_type': 'duplicate'}),
+                                     'inversion_type': 'duplicate',
+                                     'update_bounds': True}),
     'mass': (RescaleToBounds, {'update_bounds': True}),
 }
 
