272506d853e102b1a549fba82fb4a9e113bdb430
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-03-05 15:01:53 +0000

    Add poolsize to analytic proposal

--- nessai/proposal/analytic.py
+++ nessai/proposal/analytic.py
@@ -16,14 +16,22 @@ class AnalyticProposal(Proposal):
     This assumes the `new_point` method of the model draws points
     from the prior
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, poolsize=1000, **kwargs):
         super(AnalyticProposal, self).__init__(*args, **kwargs)
         self.populated = False
+        self._poolsize = poolsize
 
-    def populate(self, N=1000):
+    @property
+    def poolsize(self):
+        """Poolsize used for drawing new samples in batches."""
+        return self._poolsize
+
+    def populate(self, N=None):
         """
         Populate the pool by drawing from the priors
         """
+        if N is None:
+            N = self.poolsize
         self.samples = self.model.new_point(N=N)
         self.samples['logP'] = self.model.log_prior(self.samples)
         self.indices = np.random.permutation(self.samples.shape[0]).tolist()
