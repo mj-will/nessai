602dd2371ae9a3e19c7623fddf452cf368b01b5d
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-03-26 17:56:19 +0000

    Use mock to remove need to init class

--- tests/test_proposal/test_flowproposal/test_configuration.py
+++ tests/test_proposal/test_flowproposal/test_configuration.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 """Test general configuration functions"""
 import pytest
+from unittest.mock import create_autospec
 
 from nessai.proposal import FlowProposal
 
 
 @pytest.fixture()
-def proposal(model):
-    return FlowProposal(model, poolsize=1000)
+def proposal():
+    return create_autospec(FlowProposal)
 
 
 def test_config_drawsize_none(proposal):
     """Test the popluation configuration with no drawsize given"""
-    proposal.configure_population(2000, None, True, 10, 1.0, 0.0, 'gaussian')
+    FlowProposal.configure_population(proposal, 2000, None, True, 10, 1.0, 0.0,
+                                      'gaussian')
     assert proposal.drawsize == 2000
 
 
@@ -21,8 +23,8 @@ def test_config_poolsize_none(proposal):
     Test the popluation configuration raises an error if poolsize is None.
     """
     with pytest.raises(RuntimeError) as excinfo:
-        proposal.configure_population(
-            None, None, True, 10, 1.0, 0.0, 'gaussian')
+        FlowProposal.configure_population(
+            proposal, None, None, True, 10, 1.0, 0.0, 'gaussian')
 
     assert 'poolsize' in str(excinfo.value)
 
@@ -30,7 +32,7 @@ def test_config_poolsize_none(proposal):
 @pytest.mark.parametrize('fixed_radius', [False, 5.0, 1])
 def test_config_fixed_radius(proposal, fixed_radius):
     """Test the configuration for a fixed radius"""
-    proposal.configure_fixed_radius(fixed_radius)
+    FlowProposal.configure_fixed_radius(proposal, fixed_radius)
     assert proposal.fixed_radius == fixed_radius
 
 
@@ -39,5 +41,5 @@ def test_config_fixed_radius_not_float(proposal):
     Test the fixed radius is disabled when the radius cannot be converted to
     a float.
     """
-    proposal.configure_fixed_radius(None)
+    FlowProposal.configure_fixed_radius(proposal, None)
     assert proposal.fixed_radius is False
