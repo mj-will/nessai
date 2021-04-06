f09340215a5d28215521343381121dc8ed9be51f
Michael Williams <michaeljw1@googlemail.com>
2021-04-06 16:05:44 +0100

    Add some tests for reparameterisations

---
+++ tests/test_proposal/test_flowproposal/test_reparameterisation.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Test methods related to reparameterisations"""
+from nessai.proposal import FlowProposal
+from nessai.reparameterisations import (
+    get_reparameterisation,
+    NullReparameterisation
+)
+import pytest
+from unittest.mock import MagicMock, patch
+
+
+def test_default_reparameterisation(proposal):
+    """Test to make sure default reparameterisation does not cause errors
+    for default proposal.
+    """
+    FlowProposal.add_default_reparameterisations(proposal)
+
+
+@patch('nessai.reparameterisations.get_reparameterisation')
+def test_get_reparamaterisation(mocked_fn, proposal):
+    """Make sure the underlying function is called"""
+    FlowProposal.get_reparameterisation(proposal, 'angle')
+    assert mocked_fn.called_once_with('angle')
+
+
+@patch('nessai.reparameterisations.CombinedReparameterisation')
+def test_configure_reparameterisations(mocked_class, proposal):
+    """Test configuration for reparameterisations dictionary"""
+    proposal.add_default_reparameterisations = MagicMock()
+    proposal.get_reparameterisation = get_reparameterisation
+    proposal.model = MagicMock
+    proposal.model.bounds = {'x': [-1, 1], 'y': [-1, 1]}
+    proposal.names = ['x', 'y']
+    FlowProposal.configure_reparameterisations(
+        proposal, {'x': {'reparameterisation': 'default'}})
+
+    proposal.add_default_reparameterisations.assert_called_once()
+    assert proposal.rescaled_names == ['x_prime', 'y']
+    assert proposal.rescale_parameters == ['x']
+    assert proposal._reparameterisation.parameters == ['x', 'y']
+    assert proposal._reparameterisation.prime_parameters == ['x_prime', 'y']
+    assert mocked_class.called_once
+
+
+def test_configure_reparameterisations_incorrect_type(proposal):
+    """Assert an error is raised when input is not a dictionary"""
+    with pytest.raises(TypeError) as excinfo:
+        FlowProposal.configure_reparameterisations(proposal, ['default'])
+    assert 'must be a dictionary' in str(excinfo.value)
+
