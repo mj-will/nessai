ec3e6de3a3f9f99024d57d0c7400f75b6995534c
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-04-06 17:15:58 +0100

    Add more tests

--- tests/test_proposal/test_flowproposal/test_reparameterisation.py
+++ tests/test_proposal/test_flowproposal/test_reparameterisation.py
@@ -2,8 +2,7 @@
 """Test methods related to reparameterisations"""
 from nessai.proposal import FlowProposal
 from nessai.reparameterisations import (
-    get_reparameterisation,
-    NullReparameterisation
+    get_reparameterisation
 )
 import pytest
 from unittest.mock import MagicMock, patch
@@ -24,7 +23,7 @@ def test_get_reparamaterisation(mocked_fn, proposal):
 
 
 @patch('nessai.reparameterisations.CombinedReparameterisation')
-def test_configure_reparameterisations(mocked_class, proposal):
+def test_configure_reparameterisations_dict(mocked_class, proposal):
     """Test configuration for reparameterisations dictionary"""
     proposal.add_default_reparameterisations = MagicMock()
     proposal.get_reparameterisation = get_reparameterisation
@@ -42,9 +41,46 @@ def test_configure_reparameterisations(mocked_class, proposal):
     assert mocked_class.called_once
 
 
+@patch('nessai.reparameterisations.CombinedReparameterisation')
+def test_configure_reparameterisations_str(mocked_class, proposal):
+    """Test configuration for reparameterisations dictionary from a str"""
+    proposal.add_default_reparameterisations = MagicMock()
+    proposal.get_reparameterisation = get_reparameterisation
+    proposal.model = MagicMock
+    proposal.model.bounds = {'x': [-1, 1], 'y': [-1, 1]}
+    proposal.names = ['x', 'y']
+    FlowProposal.configure_reparameterisations(
+        proposal, {'x': 'default'})
+
+    proposal.add_default_reparameterisations.assert_called_once()
+    assert proposal.rescaled_names == ['x_prime', 'y']
+    assert proposal.rescale_parameters == ['x']
+    assert proposal._reparameterisation.parameters == ['x', 'y']
+    assert proposal._reparameterisation.prime_parameters == ['x_prime', 'y']
+    assert mocked_class.called_once
+
+
+@patch('nessai.reparameterisations.CombinedReparameterisation')
+def test_configure_reparameterisations_dict_reparam(mocked_class, proposal):
+    """Test configuration for reparameterisations dictionary"""
+    proposal.add_default_reparameterisations = MagicMock()
+    proposal.get_reparameterisation = get_reparameterisation
+    proposal.model = MagicMock
+    proposal.model.bounds = {'x': [-1, 1], 'y': [-1, 1]}
+    proposal.names = ['x', 'y']
+    FlowProposal.configure_reparameterisations(
+        proposal, {'default': {'parameters': ['x']}})
+
+    proposal.add_default_reparameterisations.assert_called_once()
+    assert proposal.rescaled_names == ['x_prime', 'y']
+    assert proposal.rescale_parameters == ['x']
+    assert proposal._reparameterisation.parameters == ['x', 'y']
+    assert proposal._reparameterisation.prime_parameters == ['x_prime', 'y']
+    assert mocked_class.called_once
+
+
 def test_configure_reparameterisations_incorrect_type(proposal):
     """Assert an error is raised when input is not a dictionary"""
     with pytest.raises(TypeError) as excinfo:
         FlowProposal.configure_reparameterisations(proposal, ['default'])
     assert 'must be a dictionary' in str(excinfo.value)
-
