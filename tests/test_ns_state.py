b4e474d0d3fdf78754669ad70a02f50ae66403f8
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-05-05 12:23:58 +0100

    Update state tests

--- tests/test_ns_state.py
+++ tests/test_ns_state.py
@@ -13,69 +13,103 @@ def nlive():
     return 100
 
 
-def test_increment(nlive):
+@pytest.fixture
+def point():
+    return {'logL': -10.0, 'logW': 0.}
+
+
+@pytest.fixture
+def second_point():
+    return {'logL': -5.0, 'logW': 0.}
+
+
+def test_increment(point, nlive):
     """Test the basic functionality of incrementing the evidence estimate"""
     state = _NSIntegralState(nlive)
-    state.increment(-10)
+    state.increment(point)
 
-    assert state.logw == (-1 / nlive)
+    assert np.isclose(state.logX, float(-1 / nlive))
     assert state.logZ != -np.inf
     np.testing.assert_equal(state.logLs, [-np.inf, -10])
 
 
-def test_finalise(nlive):
+def test_increment_w_weights(point, nlive):
+    """Test incrementing the evidence estimate when using weights"""
+    state = _NSIntegralState(nlive)
+    point['logW'] = -5.
+    state.increment(point, log_w_norm=-0.5)
+
+    assert np.isclose(state.logX, -np.exp(-4.5))
+    assert state.logZ != -np.inf
+    np.testing.assert_equal(state.logLs, [-np.inf, -10])
+
+
+def test_increment_weights_error(point, nlive):
+    """
+    Test to make sure an error is raised if the weights are non-zero when
+    the normalisation has not been given (rejection sampling).
+    """
+    state = _NSIntegralState(nlive)
+    point['logW'] = -5.
+    with pytest.raises(ValueError) as excinfo:
+        state.increment(point)
+
+    assert 'Weights must be zero' in str(excinfo.value)
+
+
+def test_finalise(point, nlive):
     """Test the check that finalise returns an improved logZ"""
     state = _NSIntegralState(nlive)
-    state.increment(-10)
+    state.increment(point)
     pre = state.logZ
     state.finalise()
     assert state.logZ != -np.inf
     assert pre != state.logZ
 
 
-def test_info(nlive):
+def test_info(point, second_point, nlive):
     """Test to check the information increases as expected"""
     state = _NSIntegralState(nlive)
-    state.increment(-10)
+    state.increment(point)
     assert state.info == [0.]
-    state.increment(-5)
+    state.increment(second_point)
     assert state.info[1] > 0
 
 
-def test_track_gradients(nlive):
+def test_track_gradients(point, second_point, nlive):
     """
     Test to make sure gradients are not computed when tracking is disabled
     """
     state = _NSIntegralState(nlive, track_gradients=False)
-    state.increment(-10)
-    state.increment(-5)
+    state.increment(point)
+    state.increment(second_point)
     assert len(state.gradients) == 1
 
 
-def test_variable_nlive(nlive):
+def test_variable_nlive(point, nlive):
     """
     Test to make sure that the using a different nlive changes the update
     values.
     """
     state = _NSIntegralState(nlive)
-    state.increment(-10, nlive=50)
-    assert state.logw == (-1 / 50)
+    state.increment(point, nlive=50)
+    assert np.isclose(state.logX, float(-1 / 50))
 
 
-def test_plot(nlive):
+def test_plot(point, second_point, nlive):
     """Test the plotting function"""
     state = _NSIntegralState(nlive)
-    state.increment(-10)
-    state.increment(-5)
+    state.increment(point)
+    state.increment(second_point)
     fig = state.plot()
     assert fig is not None
 
 
-def test_plot_w_filename(nlive, tmpdir):
+def test_plot_w_filename(point, second_point, nlive, tmpdir):
     """Test the plotting function with a filename specified"""
     filename = str(tmpdir.mkdir('test'))
     state = _NSIntegralState(nlive)
-    state.increment(-10)
-    state.increment(-5)
+    state.increment(point)
+    state.increment(second_point)
     fig = state.plot(filename=filename)
     assert fig is None
