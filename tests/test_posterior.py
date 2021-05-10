a7c2861c15d4a45efea5ed0b7682264aebce2222
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-05-10 09:37:31 +0100

    Update test for logsubexp

--- tests/test_posterior.py
+++ tests/test_posterior.py
@@ -1,13 +1,23 @@
+# -*- coding: utf-8 -*-
 import pytest
+import numpy as np
 
 from nessai.posterior import (
-        logsubexp
-        )
+    logsubexp,
+    LogNegativeError
+)
+
+
+def test_logsubexp():
+    """Test the values returned by logsubexp"""
+    out = logsubexp(2, 1)
+    np.testing.assert_almost_equal(out, np.log(np.exp(2) - np.exp(1)),
+                                   decimal=12)
 
 
 def test_logsubexp_negative():
     """
     Test behaviour of logsubexp for x < y
     """
-    with pytest.raises(Exception):
+    with pytest.raises(LogNegativeError):
         logsubexp(1, 2)
