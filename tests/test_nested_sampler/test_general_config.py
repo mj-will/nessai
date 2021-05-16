100932f440a584127ec72e0c7214a20dd67e79c5
Michael Williams <michaeljw1@googlemail.com>
2021-05-16 18:02:07 +0100

    Add more tests

--- tests/test_nested_sampler/test_general_config.py
+++ tests/test_nested_sampler/test_general_config.py
@@ -3,6 +3,8 @@
 Test general config functions called when the nested sampler is initialised.
 """
 import os
+import numpy as np
+import pytest
 from unittest.mock import patch, MagicMock
 
 from nessai.nestedsampler import NestedSampler
@@ -14,8 +16,13 @@ def test_init(sampler, model):
     sampler.configure_flow_reset = MagicMock()
     sampler.configure_flow_proposal = MagicMock()
     sampler.configure_uninformed_proposal = MagicMock
-    NestedSampler.__init__(sampler, model, nlive=1000, poolsize=1000)
+    NestedSampler.__init__(
+        sampler,
+        model,
+        nlive=100,
+        poolsize=100)
     assert sampler.initialised is False
+    assert sampler.nlive == 100
 
 
 @patch('numpy.random.seed')
@@ -60,3 +67,28 @@ def test_setup_output_w_resume(sampler, tmpdir):
     resume_file = \
         NestedSampler.setup_output(sampler, f'{p}/tests', 'resume.pkl')
     assert resume_file == f'{p}/tests/resume.pkl'
+
+
+def test_configure_max_iteration(sampler):
+    """Test to make sure the maximum iteration is set correctly"""
+    NestedSampler.configure_max_iteration(sampler, 10)
+    assert sampler.max_iteration == 10
+
+
+def test_configure_no_max_iteration(sampler):
+    """Test to make sure if no max iteration is given it is set to inf"""
+    NestedSampler.configure_max_iteration(sampler, None)
+    assert sampler.max_iteration == np.inf
+
+
+def test_training_frequency(sampler):
+    """Make sure training frequency is set"""
+    NestedSampler.configure_training_frequency(sampler, 100)
+    assert sampler.training_frequency == 100
+
+
+@pytest.mark.parametrize('f', [None, 'inf', 'None'])
+def test_training_frequency_on_empty(sampler, f):
+    """Test the values that should give 'train on empty'"""
+    NestedSampler.configure_training_frequency(sampler, f)
+    assert sampler.training_frequency == np.inf
