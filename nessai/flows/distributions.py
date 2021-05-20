d71ea5eee1ae7cf3cacf7c3361878cf641806d20
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-05-20 16:11:17 +0100

    Add Nball and TruncatedGaussian

--- nessai/flows/distributions.py
+++ nessai/flows/distributions.py
@@ -2,12 +2,18 @@
 """
 Distributions to use as the 'base distribution' for normalising flows.
 """
+import logging
+import math
 
 from nflows.distributions import Distribution
 from nflows.distributions.uniform import BoxUniform as BaseBoxUniform
 from nflows.utils import torchutils
 import numpy as np
+from scipy.special import gamma
 import torch
+
+
+logger = logging.getLogger(__name__)
 
 
 class MultivariateNormal(Distribution):
@@ -61,12 +67,121 @@ class MultivariateNormal(Distribution):
             raise NotImplementedError
 
 
+class SphericalTruncatedNormal(Distribution):
+    """
+    A Gaussian distribution truncated to lie within an n-ball of radius r.
+
+    Based on the implentation by toshas: \
+        https://github.com/toshas/torch_truncnorm
+
+    Parameters
+    ----------
+    shape : int or tuple
+        Shape of the inputs
+    r : float
+        Radius of the n-ball
+    """
+    def __init__(self, shape, r):
+        super().__init__()
+        if isinstance(shape, int):
+            shape = (shape,)
+        self._shape = torch.Size(shape)
+
+        logger.debug(f'Truncated normal with r={r:.3}')
+
+        self.register_buffer('r', torch.tensor(r, dtype=torch.float64))
+
+        self.register_buffer(
+            "_log_z",
+            (
+                np.prod(self._shape)
+                * (-0.5 * np.log(2 * np.pi)
+                    - torch.log(self._phi(self.r) - self._phi(-self.r)))
+            ),
+            persistent=False)
+
+    @staticmethod
+    def _phi(x):
+        return 0.5 * (1.0 + (x / math.sqrt(2.0)).erf())
+
+    def _log_prob(self, inputs, context):
+        if inputs.shape[1:] != self._shape:
+            raise ValueError(
+                "Expected input of shape {}, got {}".format(
+                    self._shape, inputs.shape[1:]
+                )
+            )
+        ib = self.r.gt(torch.linalg.norm(inputs, dim=1)).type_as(inputs)
+        return torch.log(ib) + self._log_z \
+            - 0.5 * torchutils.sum_except_batch(inputs ** 2, num_batch_dims=1)
+
+    def _sample(self, num_samples, context):
+        raise RuntimeError
+
+
 class BoxUniform(BaseBoxUniform):
     """Wrapper to `nflows.distributions.uniform.BoxUniform`"""
 
     def sample(self, n=1):
         """Sample from the box uniform"""
         return super().sample((n,))
+
+    def sample_and_log_prob(self, n=1):
+        """Sample from the distribution and compute the log prob"""
+        x = self.sample(n)
+        log_prob = self.log_prob(x)
+        return x, log_prob
+
+
+class UniformNBall(Distribution):
+    """
+    Uniform distribution in an n-ball
+    """
+    def __init__(self, shape, radius=1.0):
+        super().__init__()
+        if isinstance(shape, int):
+            shape = (shape,)
+        self._shape = torch.Size(shape)
+
+        self.register_buffer(
+            '_radius',
+            torch.tensor(float(radius))
+        )
+
+        self.register_buffer(
+            '_radius_sq',
+            torch.tensor(float(radius) ** 2)
+        )
+        self.register_buffer(
+            '_log_vol',
+            torch.tensor(
+                shape[-1] / 2 * np.log(np.pi) +
+                shape[-1] * np.log(radius) -
+                np.log(gamma(shape[-1] / 2 + 1))
+            )
+        )
+
+    def _log_prob(self, inputs, context):
+        if inputs.shape[1:] != self._shape:
+            raise ValueError(
+                "Expected input of shape {}, got {}".format(
+                    self._shape, inputs.shape[1:]
+                )
+            )
+        ib = self._radius_sq.gt(
+            torch.pow(inputs, 2.0).sum(dim=-1)).type_as(inputs)
+        return torch.log(ib) - self._log_vol
+
+    def _sample(self, n, context):
+        if context is None:
+            pass
+            x = torch.randn(n, *self._shape, device=self._log_vol.device)
+            r = torch.rand(
+                n, 1, *self._shape[:-1], device=self._log_vol.device)
+            s = r ** (1 / self._shape[-1]) * x / torch.linalg.norm(x)
+            return self._radius * s
+        else:
+            raise RuntimeError
 
     def sample_and_log_prob(self, n=1):
         """Sample from the distribution and compute the log prob"""
