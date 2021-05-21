d3741d6a6c64d5e85e10cf097bb462b708fc61f9
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-05-21 17:37:28 +0100

    Add tests for flow

---
+++ tests/test_nested_sampler/test_flow.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Test the functions related to the flow in the proposal.
+"""
+import pytest
+from unittest.mock import MagicMock
+from nessai.nestedsampler import NestedSampler
+
+
+def test_configure_flow_reset_false(sampler):
+    """Assert the attributes evalute to false if the inputs are false"""
+    NestedSampler.configure_flow_reset(sampler, False, False)
+    assert not sampler.reset_weights
+    assert not sampler.reset_permutations
+
+
+@pytest.mark.parametrize('weights', [10, 5.0])
+@pytest.mark.parametrize('permutations', [10, 5.0])
+def test_configure_flow_reset(sampler, weights, permutations):
+    """Assert the attributes evalute to false if the inputs are false"""
+    NestedSampler.configure_flow_reset(sampler, weights, permutations)
+    assert sampler.reset_weights == float(weights)
+    assert sampler.reset_permutations == float(permutations)
+
+
+def test_configure_flow_reset_error_weights(sampler):
+    """Assert an error is raised in the weights input is invalid"""
+    with pytest.raises(TypeError) as excinfo:
+        NestedSampler.configure_flow_reset(sampler, None, 5)
+    assert 'weights` must be' in str(excinfo.value)
+
+
+def test_configure_flow_reset_error_permutations(sampler):
+    """Assert an error is raised in the permutations input is invalid"""
+    with pytest.raises(TypeError) as excinfo:
+        NestedSampler.configure_flow_reset(sampler, 5, None)
+    assert 'permutations` must be' in str(excinfo.value)
+
+
+def test_check_training_not_completed_training(sampler):
+    """
+    Assert the flow is forced to train if training did not complete when
+    the sampler was checkpointed.
+    """
+    sampler.completed_training = False
+    train, force = NestedSampler.check_training(sampler)
+    assert train is True
+    assert force is True
+
+
+def test_check_training_train_on_empty(sampler):
+    """
+    Assert the flow is forced to train if training the pool is empty and
+    `train_on_empty` is true but the proposal was not in the process of
+    popluating.
+    """
+    sampler.completed_training = True
+    sampler.train_on_empty = True
+    sampler.proposal = MagicMock()
+    sampler.proposal.populated = False
+    sampler.proposal.populating = False
+    train, force = NestedSampler.check_training(sampler)
+    assert train is True
+    assert force is True
+
+
+def test_check_training_acceptance(sampler):
+    """
+    Assert that training will be true but not forced if the acceptance
+    threshold is met and retraining on acceptance is enabled.
+    """
+    sampler.completed_training = True
+    sampler.train_on_empty = True
+    sampler.proposal = MagicMock()
+    sampler.proposal.populated = True
+    sampler.proposal.populating = False
+    sampler.acceptance_threshold = 0.1
+    sampler.mean_block_acceptance = 0.01
+    sampler.retrain_acceptance = True
+    train, force = NestedSampler.check_training(sampler)
+    assert train is True
+    assert force is False
+
+
+def test_check_training_iteration(sampler):
+    """
+    Assert that training will be true but not forced if a training iteration
+    is reached (n iterations have passed since last updated).
+    """
+    sampler.completed_training = True
+    sampler.train_on_empty = True
+    sampler.proposal = MagicMock()
+    sampler.proposal.populated = True
+    sampler.proposal.populating = False
+    sampler.acceptance_threshold = 0.1
+    sampler.mean_block_acceptance = 0.2
+    sampler.retrain_acceptance = False
+    sampler.iteration = 3521
+    sampler.last_updated = 2521
+    sampler.training_frequency = 1000
+    train, force = NestedSampler.check_training(sampler)
+    assert train is True
+    assert force is False
+
+
+@pytest.mark.parametrize('config', [
+    dict(),
+    dict(train_on_empty=False, populated=False),
+    dict(train_on_empty=True, populated=False, populating=True),
+    dict(mean_acceptance=0.01, acceptance_threshold=0.5,
+         retrain_acceptance=False),
+    dict(mean_acceptance=0.5, acceptance_threshold=0.01,
+         retrain_acceptance=True),
+    dict(iteration=800, last_updated=0, training_frequency=801)
+])
+def test_check_training_false(sampler, config):
+    """
+    Test a range of different scenarios that should all not start training.
+    """
+    sampler.completed_training = True
+    sampler.train_on_empty = config.get('train_on_empty', False)
+    sampler.proposal = MagicMock()
+    sampler.proposal.populated = config.get('populated', False)
+    sampler.proposal.populating = config.get('populating', False)
+    sampler.acceptance_threshold = config.get('acceptance_threshold', 0.1)
+    sampler.mean_block_acceptance = config.get('mean_acceptance', 0.2)
+    sampler.retrain_acceptance = config.get('retrain_acceptance', False)
+    sampler.iteration = config.get('iteration', 3000)
+    sampler.last_updated = config.get('last_updated', 2500)
+    sampler.training_frequency = config.get('training_frequency', 1000)
+    train, force = NestedSampler.check_training(sampler)
+    assert train is False
+    assert force is False
