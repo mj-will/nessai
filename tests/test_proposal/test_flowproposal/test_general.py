e7f1523f0d3be5dd84333f25a998b77de202954d
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-06-02 18:00:42 +0100

    Improve tests for plot_pool

--- tests/test_proposal/test_flowproposal/test_general.py
+++ tests/test_proposal/test_flowproposal/test_general.py
@@ -3,7 +3,8 @@
 import os
 import numpy as np
 import pytest
-from unittest.mock import MagicMock
+import torch
+from unittest.mock import MagicMock, patch
 
 from nessai.proposal import FlowProposal
 from nessai.livepoint import numpy_array_to_live_points
@@ -98,12 +99,57 @@ def test_training_plots(proposal, tmpdir, plot):
     assert os.path.exists(f'{output}/x_prime_generated.png') is bool(plot)
 
 
-def test_pool_plot_all(proposal, tmpdir):
+def test_plot_pool_all(proposal):
     """Test for the plots that show the pool of samples"""
-    output = tmpdir.mkdir('test_pool_plot')
-    proposal.output = output
+    proposal.output = 'test'
     proposal._plot_pool = 'all'
     proposal.populated_count = 0
     x = numpy_array_to_live_points(np.random.randn(10, 2), ['x', 'y'])
-    FlowProposal.plot_pool(proposal, None, x)
-    assert os.path.exists(f'{output}/pool_0.png')
+    with patch('nessai.proposal.flowproposal.plot_live_points') as plot:
+        FlowProposal.plot_pool(proposal, None, x)
+    plot.assert_called_once_with(x, c='logL', filename='test/pool_0.png')
+
+
+@pytest.mark.parametrize('alt_dist', [False, True])
+def test_plot_pool_1d(proposal, tmpdir, alt_dist):
+    """Test `plot_pool` when ploting is not 'all'.
+
+    Test cases when there is an alternative base distibution is used and
+    when one is not used.
+    """
+    output = tmpdir.mkdir('test_plot_pool_1d')
+    proposal.output = output
+    proposal._plot_pool = True
+    proposal.populated_count = 0
+
+    z = np.random.randn(10, 2)
+    x = numpy_array_to_live_points(np.random.randn(10, 2), ['x', 'y'])
+    training_data = \
+        numpy_array_to_live_points(np.random.randn(10, 2), ['x', 'y'])
+    proposal.training_data = training_data
+    log_p = torch.arange(10)
+
+    proposal.flow = MagicMock()
+    proposal.flow.device = 'cpu'
+    if alt_dist:
+        proposal.alt_dist = MagicMock()
+        proposal.alt_dist.log_prob = MagicMock(return_value=log_p)
+    else:
+        proposal.flow.model.base_distribution_log_prob = \
+            MagicMock(return_value=log_p)
+        proposal.alt_dist = None
+    with patch('nessai.proposal.flowproposal.plot_1d_comparison') as plot:
+        FlowProposal.plot_pool(proposal, z, x)
+
+    plot.assert_called_once_with(
+        training_data,
+        x,
+        labels=['live points', 'pool'],
+        filename=f'{output}/pool_0.png'
+    )
+    assert os.path.exists(f'{output}/pool_0_log_q.png')
+
+    if alt_dist:
+        proposal.alt_dist.log_prob.assert_called_once()
+    else:
+        proposal.flow.model.base_distribution_log_prob.assert_called_once()
