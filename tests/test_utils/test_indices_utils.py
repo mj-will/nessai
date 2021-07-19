73552d75791d1ed0e5a69b2f2bbd53b2594dfb49
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-07-19 10:57:59 +0100

    Add test for bonferroni correction

--- tests/test_utils/test_indices_utils.py
+++ tests/test_utils/test_indices_utils.py
@@ -5,7 +5,7 @@ Tests for utilies related to insertion indices.
 import numpy as np
 import pytest
 
-from nessai.utils.indices import compute_indices_ks_test
+from nessai.utils.indices import compute_indices_ks_test, bonferroni_correction
 
 
 @pytest.mark.parametrize("mode", ['D+', 'D-'])
@@ -33,3 +33,12 @@ def test_ks_test_empty_indices():
     """
     out = compute_indices_ks_test([], 1000, mode='D+')
     assert all(o is None for o in out)
+
+
+def test_bonferroni_correction():
+    """Test the Bonferroni correction for p-values"""
+    p_values = np.linspace(0, 0.5, 4)
+    rejected, corrected, alpha = bonferroni_correction(p_values)
+    np.testing.assert_array_equal(corrected, np.array([0, 2 / 3, 1, 1]))
+    assert rejected.tolist() == [True, False, False, False]
+    assert alpha == 0.0125
