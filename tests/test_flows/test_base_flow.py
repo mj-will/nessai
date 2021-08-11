a953f0292aa23b346e5283ce31400194f8715380
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-08-11 11:39:12 +0100

    Fix tests for base flow

--- tests/test_flows/test_base_flow.py
+++ tests/test_flows/test_base_flow.py
@@ -3,17 +3,30 @@
 Test the base flow class
 """
 import pytest
+from unittest.mock import create_autospec
 
 from nessai.flows import BaseFlow
 
 
-@pytest.mark.parametrize('method', ['forward', 'inverse', 'sample', 'log_prob',
-                                    'base_distribution_log_prob',
-                                    'forward_and_log_prob',
-                                    'sample_and_log_prob'])
-def test_base_flow_methods(method):
+@pytest.fixture
+def flow():
+    return create_autospec(BaseFlow)
+
+
+def test_base_flow_abstract_methods():
+    """Assert an error is raised if the methods are not implemented."""
+    with pytest.raises(TypeError) as excinfo:
+        BaseFlow()
+    assert 'instantiate abstract class BaseFlow with abstract method' \
+        in str(excinfo.value)
+
+
+@pytest.mark.parametrize(
+    'method',
+    ['forward', 'inverse', 'sample', 'log_prob', 'base_distribution_log_prob',
+     'forward_and_log_prob', 'sample_and_log_prob']
+)
+def test_base_flow_methods(method, flow):
     """Test to make sure all of the method in the base class raise errors"""
-    m = getattr(BaseFlow(), method)
-    assert m
     with pytest.raises(NotImplementedError):
-        m(None)
+        getattr(BaseFlow, method)(flow, None)
