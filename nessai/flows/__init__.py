65cefd868031644417e7a05aa2f21826e1103192
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-09-03 12:11:51 +0100

    Update to include NLSq

--- nessai/flows/__init__.py
+++ nessai/flows/__init__.py
@@ -5,15 +5,19 @@ implementations.
 """
 from .base import BaseFlow, NFlow
 from .maf import MaskedAutoregressiveFlow
+from .nlsq import NonLinearSquaredFlow
 from .nsf import NeuralSplineFlow
 from .realnvp import RealNVP
 from .utils import configure_model, reset_weights, reset_permutations
 
-__all__ = ["BaseFlow",
-           "NFlow",
-           "MaskedAutoregressiveFlow",
-           "NeuralSplineFlow",
-           "RealNVP",
-           "configure_model",
-           "reset_weights",
-           "reset_permutations"]
+__all__ = [
+    "BaseFlow",
+    "NFlow",
+    "MaskedAutoregressiveFlow",
+    "NeuralSplineFlow",
+    "NonLinearSquaredFlow",
+    "RealNVP",
+    "configure_model",
+    "reset_weights",
+    "reset_permutations",
+]
