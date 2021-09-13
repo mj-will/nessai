0f3bce466cb26e4c02c584c629889d952c7997a2
Michael J. Williams <michaeljw1@googlemail.com>
2021-09-13 16:53:25 +0100

    Improve various tests for AnglePair

--- tests/test_reparameterisations/test_angle_pair.py
+++ tests/test_reparameterisations/test_angle_pair.py
@@ -3,15 +3,19 @@
 Test the AnglePair reparmeterisation
 """
 import numpy as np
-from numpy.testing import assert_equal
 import pytest
+from unittest.mock import MagicMock, create_autospec
 
 from nessai.reparameterisations import AnglePair
-from nessai.livepoint import get_dtype
+from nessai.livepoint import get_dtype, parameters_to_live_point
 
-angle_pairs = [(['ra', 'dec'], [[0, 2 * np.pi], [-np.pi / 2, np.pi / 2]]),
-               (['az', 'zen'], [[0, 2 * np.pi], [0, np.pi]]),
-               (['zen', 'az'], [[0, np.pi], [0, 2 * np.pi]])]
+angle_pairs = [
+    (['ra', 'dec'], [[0, 2 * np.pi], [-np.pi / 2, np.pi / 2]]),
+    (['dec', 'ra'], [[0, 2 * np.pi], [-np.pi / 2, np.pi / 2]]),
+    (['ra', 'dec'], [[-np.pi, np.pi], [-np.pi / 2, np.pi / 2]]),
+    (['az', 'zen'], [[0, 2 * np.pi], [0, np.pi]]),
+    (['zen', 'az'], [[0, np.pi], [0, 2 * np.pi]])
+]
 
 
 @pytest.fixture(params=angle_pairs, scope='function')
@@ -88,8 +92,7 @@ def test_two_angles(angles):
         assert reparam.convention == 'az-zen'
 
     # Make sure parameter[0] is always ra or azimuth
-    assert_equal(reparam.prior_bounds[reparam.angles[0]],
-                 np.array([0, 2 * np.pi]))
+    assert np.ptp(reparam.prior_bounds[reparam.angles[0]]) == 2 * np.pi
 
     assert reparam.chi is not False
     assert hasattr(reparam.chi, 'rvs')
@@ -154,3 +157,233 @@ def test_w_radial(assert_invertibility):
     radial = np.random.uniform(*prior_bounds['r'], n)
 
     assert assert_invertibility(reparam, angles, radial=radial)
+
+
+@pytest.mark.parametrize(
+    'convention, input, expected',
+    [
+        ['az-zen', (1, 0, 0), (0, np.pi / 2, 1)],
+        ['ra-dec', (1, 0, 0), (0, 0, 1)],
+        ['az-zen', (-1, 0, 0), (np.pi, np.pi / 2, 1)],
+        ['ra-dec', (-1, 0, 0), (np.pi, 0, 1)],
+        ['az-zen', (0, 1, 0), (np.pi / 2, np.pi / 2, 1)],
+        ['ra-dec', (0, 1, 0), (np.pi / 2, 0, 1)],
+        ['az-zen', (0, -1, 0), (3 * np.pi / 2, np.pi / 2, 1)],
+        ['ra-dec', (0, -1, 0), (3 * np.pi / 2, 0, 1)],
+        ['az-zen', (0, 0, 1), (0, 0, 1)],
+        ['ra-dec', (0, 0, 1), (0, np.pi / 2, 1)],
+        ['az-zen', (0, 0, -1), (0, np.pi, 1)],
+        ['ra-dec', (0, 0, -1), (0, -np.pi / 2, 1)],
+        ['az-zen', (0, 0, 0), (0, 0, 0)],
+        ['ra-dec', (0, 0, 0), (0, 0, 0)],
+        ['az-zen', (1, 1, np.sqrt(2)), (np.pi / 4, np.pi / 4, 2)],
+        ['ra-dec', (1, 1, np.sqrt(2)), (np.pi / 4, np.pi / 4, 2)],
+        ['az-zen', (-1, -1, -np.sqrt(2)), (5 * np.pi / 4, 3 * np.pi / 4, 2)],
+        ['ra-dec', (-1, -1, -np.sqrt(2)), (5 * np.pi / 4, -np.pi / 4, 2)],
+    ]
+)
+def test_specific_points_x_prime_to_x_0_2pi(convention, input, expected):
+    """Test specific points on a sphere.
+
+    Order is (x, y, z) to (ra/az, dec/zen, r) using [0, 2pi] for ra/az.
+
+    Test:
+    - (+/-1, 0, 0) and all permutations
+    - (0, 0, 0)
+    - (+/-1, +-1, sqrt(2)) a point exactly 2 away from the origin
+    """
+    parameters = ['a', 'b']
+    if convention == 'ra-dec':
+        prior_bounds = {'a': [0, 2 * np.pi], 'b': [-np.pi / 2, np.pi / 2]}
+    else:
+        prior_bounds = {'a': [0, 2 * np.pi], 'b': [0, np.pi]}
+    reparam = AnglePair(
+        parameters=parameters,
+        prior_bounds=prior_bounds,
+        convention=convention,
+    )
+
+    x_prime = parameters_to_live_point(input, reparam.prime_parameters)
+    x = parameters_to_live_point([0, 0, 0], reparam.parameters)
+    log_j = 0
+
+    out, _, _ = reparam.inverse_reparameterise(x, x_prime, log_j)
+
+    np.testing.assert_equal(out[reparam.parameters[0]], expected[0])
+    np.testing.assert_equal(out[reparam.parameters[1]], expected[1])
+    np.testing.assert_equal(out[reparam.parameters[2]], expected[2])
+
+
+@pytest.mark.parametrize(
+    'convention, input, expected',
+    [
+        ['az-zen', (1, 0, 0), (0, np.pi / 2, 1)],
+        ['ra-dec', (1, 0, 0), (0, 0, 1)],
+        ['az-zen', (-1, 0, 0), (np.pi, np.pi / 2, 1)],
+        ['ra-dec', (-1, 0, 0), (np.pi, 0, 1)],
+        ['az-zen', (0, 1, 0), (np.pi / 2, np.pi / 2, 1)],
+        ['ra-dec', (0, 1, 0), (np.pi / 2, 0, 1)],
+        ['az-zen', (0, -1, 0), (- np.pi / 2, np.pi / 2, 1)],
+        ['ra-dec', (0, -1, 0), (- np.pi / 2, 0, 1)],
+        ['az-zen', (0, 0, 1), (0, 0, 1)],
+        ['ra-dec', (0, 0, 1), (0, np.pi / 2, 1)],
+        ['az-zen', (0, 0, -1), (0, np.pi, 1)],
+        ['ra-dec', (0, 0, -1), (0, -np.pi / 2, 1)],
+        ['az-zen', (0, 0, 0), (0, 0, 0)],
+        ['ra-dec', (0, 0, 0), (0, 0, 0)],
+        ['az-zen', (1, 1, np.sqrt(2)), (np.pi / 4, np.pi / 4, 2)],
+        ['ra-dec', (1, 1, np.sqrt(2)), (np.pi / 4, np.pi / 4, 2)],
+        ['az-zen', (-1, -1, -np.sqrt(2)), (-3 * np.pi / 4, 3 * np.pi / 4, 2)],
+        ['ra-dec', (-1, -1, -np.sqrt(2)), (-3 * np.pi / 4, -np.pi / 4, 2)],
+    ]
+)
+def test_specific_points_x_prime_to_x_pi_pi(convention, input, expected):
+    """Test specific points on a sphere.
+
+    Order is (x, y, z) to (ra/az, dec/zen, r) using [-pi, pi] for ra/az.
+
+    Test:
+    - (+/-1, 0, 0) and all permutations
+    - (0, 0, 0)
+    - (+/-1, +-1, sqrt(2)) a point exactly 2 away from the origin
+    """
+    parameters = ['a', 'b']
+    if convention == 'ra-dec':
+        prior_bounds = {'a': [-np.pi, np.pi], 'b': [-np.pi / 2, np.pi / 2]}
+    else:
+        prior_bounds = {'a': [-np.pi, np.pi], 'b': [0, np.pi]}
+    reparam = AnglePair(
+        parameters=parameters,
+        prior_bounds=prior_bounds,
+        convention=convention,
+    )
+
+    x_prime = parameters_to_live_point(input, reparam.prime_parameters)
+    x = parameters_to_live_point([0, 0, 0], reparam.parameters)
+    log_j = 0
+
+    out, _, _ = reparam.inverse_reparameterise(x, x_prime, log_j)
+
+    np.testing.assert_equal(out[reparam.parameters[0]], expected[0])
+    np.testing.assert_equal(out[reparam.parameters[1]], expected[1])
+    np.testing.assert_equal(out[reparam.parameters[2]], expected[2])
+
+
+@pytest.mark.parametrize(
+    'prior_bounds',
+    [
+        {'ra': [0, np.pi], 'dec':  [-np.pi / 2, np.pi / 2]},
+        {'ra': [0, 2 * np.pi], 'dec':  [-np.pi, np.pi]},
+    ]
+)
+def test_invalid_prior_ranges(prior_bounds):
+    """Assert an error is raised the prior ranges are invalid"""
+    with pytest.raises(ValueError) as excinfo:
+        AnglePair(parameters=['ra', 'dec'], prior_bounds=prior_bounds)
+    assert 'Invalid prior ranges' in str(excinfo.value)
+
+
+def test_invalid_prior_bounds_az():
+    """Assert an error is raised the prior bounds are invalid"""
+    prior_bounds = {'az': [np.pi, 3 * np.pi], 'zen': [0, np.pi]}
+    with pytest.raises(ValueError) as excinfo:
+        AnglePair(parameters=['az', 'zen'], prior_bounds=prior_bounds)
+    assert 'Prior bounds for az must be' in str(excinfo.value)
+
+
+def test_invalid_prior_bounds_inc():
+    """Assert an error is raised the prior bounds are invalid for the \
+        inclination angle.
+    """
+    prior_bounds = {'ra': [0.0, 2 * np.pi], 'dec': [0, np.pi]}
+    with pytest.raises(ValueError) as excinfo:
+        AnglePair(
+            parameters=['ra', 'dec'], prior_bounds=prior_bounds,
+            convention='ra-dec'
+        )
+    assert 'Prior bounds for dec must be' in str(excinfo.value)
+
+
+def test_unknown_prior():
+    """Assert an unknown prior raises an error"""
+    with pytest.raises(ValueError) as excinfo:
+        AnglePair(
+            parameters=['az', 'zen'],
+            prior_bounds={'az': [0.0, 2 * np.pi], 'zen': [0.0, np.pi]},
+            convention='az-zen',
+            prior='uniform',
+        )
+    assert "Unknown prior: `uniform`. Choose from: ['isotropic', None]" \
+        in str(excinfo.value)
+
+
+def test_unknown_convention():
+    """Assert an unknown convetion raises an error"""
+    with pytest.raises(ValueError) as excinfo:
+        AnglePair(
+            parameters=['az', 'zen'],
+            prior_bounds={'az': [0.0, 2 * np.pi], 'zen': [0.0, np.pi]},
+            convention='sky',
+        )
+    assert "Unknown convention: `sky`. Choose from: ['az-zen', 'ra-dec']" \
+        in str(excinfo.value)
+
+
+def test_log_prior():
+    """Assert log_prior calls the log pdf of a chi distribution."""
+    reparam = create_autospec(AnglePair)
+    reparam.parameters = ['a', 'b', 'r']
+    reparam.has_prior = True
+    x = parameters_to_live_point([0, 0, 2.0], reparam.parameters)
+    reparam.chi = MagicMock()
+    reparam.chi.logpdf = MagicMock(return_value=1.0)
+    out = AnglePair.log_prior(reparam, x)
+    reparam.chi.assert_not_called()
+    reparam.chi.logpdf.assert_called_once_with(x['r'])
+    assert out == 1.0
+
+
+def test_log_prior_radial():
+    """Assert log_prior raises an error if chi is False"""
+    reparam = create_autospec(AnglePair)
+    reparam.chi = False
+    reparam.has_prior = True
+    with pytest.raises(RuntimeError) as excinfo:
+        AnglePair.log_prior(reparam, 1)
+    assert 'log_prior is not defined' in str(excinfo.value)
+
+
+def test_log_prior_has_prior():
+    """Assert log_prior raises an error if has_prior is False.
+
+    This shouldn't be possible unless the user defines `chi`.
+    """
+    reparam = create_autospec(AnglePair)
+    reparam.chi = True
+    reparam.has_prior = False
+    with pytest.raises(RuntimeError) as excinfo:
+        AnglePair.log_prior(reparam, 1)
+    assert 'log_prior is not defined' in str(excinfo.value)
+
+
+def test_x_prime_log_prior():
+    """Test the x prime prior"""
+    reparam = create_autospec(AnglePair)
+    reparam.prime_parameters = ['x', 'y', 'z']
+    reparam.has_prime_prior = True
+    x = parameters_to_live_point([1, 1, 1], reparam.prime_parameters)
+    # Should be -1.5 * log(2pi) - 1.5
+    expected = -1.5 * (1 + np.log(2 * np.pi))
+    out = AnglePair.x_prime_log_prior(reparam, x)
+    np.testing.assert_equal(out, expected)
+
+
+def test_prime_prior_error():
+    """Assert an error is raised if calling the prime prior and \
+        has prime prior is false.
+    """
+    reparam = create_autospec(AnglePair)
+    reparam.has_prime_prior = False
+    with pytest.raises(RuntimeError) as excinfo:
+        AnglePair.x_prime_log_prior(reparam, 1)
+    assert 'x prime prior is not defined' in str(excinfo.value)
