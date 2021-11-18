d36b1acb5a1809061ed7f8fb4d77a694eed9b581
mj-will <m.williams.4@research.gla.ac.uk>
2021-11-18 16:26:25 +0000

    Add more functions

--- nessai/utils/structures.py
+++ nessai/utils/structures.py
@@ -2,6 +2,7 @@
 """
 Utilities for manipulating python structures such as lists and dictionaries.
 """
+import numpy as np
 
 
 def replace_in_list(target_list, targets, replacements):
@@ -31,3 +32,40 @@ def replace_in_list(target_list, targets, replacements):
     for t, r in zip(targets, replacements):
         i = target_list.index(t)
         target_list[i] = r
+
+
+def get_subset_arrays(indices, *args):
+    """Return a subset of a set of arrays.
+
+    Assumes all arrays are the same length.
+
+    Parameters
+    ----------
+    indices : array
+        Array of indices or boolean array of same length as input arrays
+    args : arrays
+        Set of arrays to index.
+    """
+    return (a[indices] for a in args)
+
+
+def isfinite_struct(x, names=None):
+    """Check for +/- infinity and NaNs in a structued array.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        Structured array
+    names : list[str]
+        Names of the fields to include. If not specified all fields are
+        checked.
+
+    Returns
+    -------
+    np.ndarray
+        Array of booleans indicating if each entry in the array is finite
+        (True) or not (False).
+    """
+    if names is None:
+        names = x.dtype.names
+    return np.all([np.isfinite(x[n]) for n in names], axis=0)
