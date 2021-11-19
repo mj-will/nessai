5dc846ab3db0a569ad216446fca813e49001f859
mj-will <m.williams.4@research.gla.ac.uk>
2021-11-19 11:55:27 +0000

    Add more information related functions

--- nessai/utils/information.py
+++ nessai/utils/information.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Utitilies for computing information and entropy"""
 import numpy as np
+from scipy.special import logsumexp
 
 
 def self_entropy(p: np.ndarray) -> float:
@@ -26,3 +27,25 @@ def efficiency(p: np.ndarray) -> float:
         Arrray of probabilities
     """
     return self_entropy(p) / np.log(len(p))
+
+
+def cumulative_entropy(p: np.ndarray, base: float = np.e) -> np.ndarray:
+    """Compute the entropy assuming a fraction of the points are discarded.
+
+    Starts with a single point and adds a point each time until all
+    of the points have been added.
+    """
+    assert p.size <= 10_000
+    p = np.array(p, dtype=np.float128)
+    norm = np.cumsum(p)
+    q = p / norm[np.newaxis, :].T
+    q[np.triu_indices(p.size, k=1)] = np.nan
+    h = -np.nansum(q * np.log(q) / np.log(base), axis=1)
+    return h
+
+
+def kl_divergence_from_log(log_p: np.ndarray, log_q: np.ndarray) -> float:
+    """KL Divergence where inputs are log-probabilties."""
+    log_p = log_p - logsumexp(log_p)
+    log_q = log_q - logsumexp(log_q)
+    return np.mean(log_p - log_q)
