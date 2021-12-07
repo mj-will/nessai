e6049b41d6e842cb4cc78c40c943e893a695e92d
mj-will <m.williams.4@research.gla.ac.uk>
2021-12-07 09:38:35 +0000

    Update init to include ImportanceFlowProposal

--- nessai/proposal/__init__.py
+++ nessai/proposal/__init__.py
@@ -5,10 +5,13 @@ Proposal methods for used in the nested sampler.
 from .analytic import AnalyticProposal
 from .augmented import AugmentedFlowProposal
 from .flowproposal import FlowProposal
+from .importance import ImportanceFlowProposal
 from .rejection import RejectionProposal
 
-__all__ = ["AnalyticProposal",
-           "AugmentedFlowProposal",
-           "FlowProposal",
-           "RejectionProposal"
-           ]
+__all__ = [
+    "AnalyticProposal",
+    "AugmentedFlowProposal",
+    "FlowProposal",
+    "ImportanceFlowProposal",
+    "RejectionProposal",
+]
