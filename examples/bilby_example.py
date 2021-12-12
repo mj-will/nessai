2aada05a27c97cabd2663870d4c83b7a060b6018
Michael J. Williams <michaeljw1@googlemail.com>
2021-12-12 13:21:58 +0000

    Update basic bilby examples

--- examples/bilby_example.py
+++ examples/bilby_example.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-# Example of using Nessai with Bilby (Requires separate installation)
+# Example of using nessai with Bilby (Requires separate installation)
 # See 2d_gaussian.py for a more detailed explanation of using nessai
 
 import bilby
 import numpy as np
 
 # The output from the sampler will be saved to:
-# '$outdir/$label_flowproposal/'
+# '$outdir/$label_nessai/'
 # alongside the usual bilby outputs
 outdir = './outdir/'
 label = 'bilby_example'
 
-# Setup the bilby logger
-bilby.core.utils.setup_logger(outdir=outdir, label=label, log_level='INFO')
+# Setup the bilby logger this will also configure the nessai logger.
+bilby.core.utils.setup_logger(outdir=outdir, label=label)
 
 # Define a likelihood using Bilby
 
@@ -21,41 +21,39 @@ bilby.core.utils.setup_logger(outdir=outdir, label=label, log_level='INFO')
 class SimpleGaussianLikelihood(bilby.Likelihood):
 
     def __init__(self):
-        """
-        A very simple Gaussian likelihood
-        """
+        """A very simple Gaussian likelihood"""
         super().__init__(parameters={'x': None, 'y': None})
 
     def log_likelihood(self):
-        x = self.parameters['x']
-        y = self.parameters['y']
-        return -0.5*(x ** 2. + y ** 2.) - np.log(2.0 * np.pi)
+        """Log-likelihood."""
+        return (
+            -0.5 * (self.parameters['x'] ** 2. + self.parameters['y'] ** 2.)
+            - np.log(2.0 * np.pi)
+        )
 
 
-# Define priors (this provides the bounds that are then used in Nessai)
-priors = dict(x=bilby.core.prior.Uniform(-10, 10, 'x'),
-              y=bilby.core.prior.Uniform(-10, 10, 'y'))
+# Define priors (this provides the bounds that are then used in nessai)
+priors = dict(
+    x=bilby.core.prior.Uniform(-10, 10, 'x'),
+    y=bilby.core.prior.Uniform(-10, 10, 'y')
+)
 
 # Instantiate the likelihood
 likelihood = SimpleGaussianLikelihood()
-
-# Configure the normalising flow
-flow_config = dict(
-        max_epochs=50,
-        patience=10,
-        model_config=dict(n_blocks=2, n_neurons=4, n_layers=2,
-                          device_tag='cpu',
-                          kwargs=dict(batch_norm_between_layers=True))
-        )
 
 # Run using bilby.run_sampler, any kwargs are parsed to the sampler
 # NOTE: when using Bilby if the priors can be sampled analytically  the flag
 # `analytic_priors` enables faster initial sampling. See 'further details' in
 # the documentation for more details
-result = bilby.run_sampler(outdir=outdir, label=label, resume=False, plot=True,
-                           likelihood=likelihood, priors=priors,
-                           sampler='nessai', nlive=1000,
-                           maximum_uninformed=2000, flow_config=flow_config,
-                           rescale_parameters=True,
-                           injection_parameters={'x': 0.0, 'y': 0.0},
-                           analytic_priors=True, seed=1234)
+result = bilby.run_sampler(
+    outdir=outdir,
+    label=label,
+    resume=False,
+    plot=True,
+    likelihood=likelihood,
+    priors=priors,
+    sampler='nessai',
+    injection_parameters={'x': 0.0, 'y': 0.0},
+    analytic_priors=True,
+    seed=1234
+)
--- examples/bilby_unbounded_priors.py
+++ examples/bilby_unbounded_priors.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
 # Example of using Nessai with Bilby (Requires separate installation)
-# with unbounded priors. We use bilby since the priors include a sample
-# method which is used to redefine `Model.new_point`. Without this we
-# would have to redefine the method ourselves.
+# with unbounded priors.
 
 import bilby
 import numpy as np
 
-# The output from the sampler will be saved to:
-# '$(outdir)/$(label)_nessai/'
-# alongside the usual bilby outputs
 outdir = './outdir/'
 label = 'bilby_unbounded_priors'
 
 # Setup the bilby logger
-bilby.core.utils.setup_logger(outdir=outdir, label=label, log_level='INFO')
+bilby.core.utils.setup_logger(outdir=outdir, label=label, log_level='WARNING')
 
 # Define a likelihood using Bilby
 
@@ -23,32 +18,25 @@ bilby.core.utils.setup_logger(outdir=outdir, label=label, log_level='INFO')
 class SimpleGaussianLikelihood(bilby.Likelihood):
 
     def __init__(self):
-        """
-        A very simple Gaussian likelihood
-        """
+        """A very simple Gaussian likelihood"""
         super().__init__(parameters={'x': None, 'y': None})
 
     def log_likelihood(self):
-        x = self.parameters['x']
-        y = self.parameters['y']
-        return -0.5*(x ** 2. + y ** 2.) - np.log(2.0 * np.pi)
+        """Log-likelihood."""
+        return (
+            -0.5 * (self.parameters['x'] ** 2. + self.parameters['y'] ** 2.)
+            - np.log(2.0 * np.pi)
+        )
 
 
 # Define priors, we'll use Gaussians since they're unbounded.
-priors = dict(x=bilby.core.prior.Gaussian(0, 5, 'x'),
-              y=bilby.core.prior.Gaussian(0, 10, 'y'))
+priors = dict(
+    x=bilby.core.prior.Gaussian(0, 5, 'x'),
+    y=bilby.core.prior.Gaussian(0, 10, 'y')
+)
 
 # Instantiate the likelihood
 likelihood = SimpleGaussianLikelihood()
-
-# Configure the normalising flow
-flow_config = dict(
-        max_epochs=50,
-        patience=10,
-        model_config=dict(n_blocks=2, n_neurons=4, n_layers=2,
-                          device_tag='cpu',
-                          kwargs=dict(batch_norm_between_layers=True))
-        )
 
 # Run using bilby.run_sampler, any kwargs are parsed to the sampler
 # NOTE: when using Bilby if the priors can be sampled analytically  the flag
@@ -57,14 +45,18 @@ flow_config = dict(
 # We need to disable the rescaling since we can't rescale without bounds.
 # Alternatively a different reparameterisation could be used, in this case
 # we can used the 'Rescale' reparameterisation that rescales the inputs by
-# a constant, this is useful when we do not prior bounds we can use.
-result = bilby.run_sampler(outdir=outdir, label=label, resume=False, plot=True,
-                           likelihood=likelihood, priors=priors,
-                           sampler='nessai', nlive=1000,
-                           maximum_uninformed=2000, flow_config=flow_config,
-                           injection_parameters={'x': 0.0, 'y': 0.0},
-                           reparameterisations={
-                               'scale': {'parameters': ['x', 'y'],
-                                         'scale': [5, 10]}},
-                           proposal_plots='min',
-                           analytic_priors=False, seed=1234)
+# a constant, this is useful when we do not have prior bounds we can use.
+result = bilby.run_sampler(
+    outdir=outdir,
+    label=label,
+    resume=False,
+    plot=True,
+    likelihood=likelihood,
+    priors=priors,
+    sampler='nessai',
+    injection_parameters={'x': 0.0, 'y': 0.0},
+    reparameterisations={
+        'scale': {'parameters': ['x', 'y'], 'scale': [5, 10]}},
+    analytic_priors=True,
+    seed=1234
+)
