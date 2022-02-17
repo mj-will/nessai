34a09392326189ad7e360bf44c33a16b2c318dfb
mj-will <m.williams.4@research.gla.ac.uk>
2022-02-17 09:35:09 +0000

    Add GW example using importance sampler

---
+++ examples/gw/importance_gw_example.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+"""
+Example of running the importance sampling version of nessai for gravitational-
+wave inference.
+
+Based on the Bilby example: https://git.ligo.org/lscsoft/bilby
+"""
+import bilby
+import numpy as np
+
+outdir = './outdir/'
+label = 'importance_gw_example'
+
+bilby.core.utils.setup_logger(outdir=outdir, label=label, log_level='WARNING')
+
+duration = 4.
+sampling_frequency = 2048.
+
+np.random.seed(170817)
+
+# Use an injection that is similar to GW150914
+injection_parameters = dict(
+    total_mass=66., mass_ratio=0.9, a_1=0.4, a_2=0.3, tilt_1=0.5, tilt_2=1.0,
+    phi_12=1.7, phi_jl=0.3, luminosity_distance=2000, theta_jn=0.4, psi=2.659,
+    phase=1.3, geocent_time=1126259642.413, ra=1.375, dec=-1.2108
+)
+
+waveform_arguments = dict(
+    waveform_approximant='IMRPhenomPv2',
+    reference_frequency=50.
+)
+
+# Create the waveform_generator
+waveform_generator = bilby.gw.waveform_generator.WaveformGenerator(
+    sampling_frequency=sampling_frequency,
+    duration=duration,
+    frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
+    parameter_conversion=(bilby.gw.conversion
+                          .convert_to_lal_binary_black_hole_parameters),
+    waveform_arguments=waveform_arguments
+)
+
+# Set up interferometers
+ifos = bilby.gw.detector.InterferometerList(['H1', 'L1', 'V1'])
+ifos.set_strain_data_from_power_spectral_densities(
+    sampling_frequency=sampling_frequency, duration=duration,
+    start_time=injection_parameters['geocent_time'] - 3
+)
+ifos.inject_signal(
+    waveform_generator=waveform_generator, parameters=injection_parameters
+)
+
+# Set up prior
+priors = bilby.gw.prior.BBHPriorDict()
+priors['geocent_time'] = bilby.core.prior.Uniform(
+    minimum=injection_parameters['geocent_time'] - 0.1,
+    maximum=injection_parameters['geocent_time'] + 0.1,
+    name='geocent_time', latex_label='$t_c$', unit='$s$'
+)
+
+# Only sample a subset of the parameters
+for key in ['a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_12', 'phi_jl',
+            'luminosity_distance', 'psi', 'geocent_time', 'ra', 'dec']:
+    priors[key] = injection_parameters[key]
+
+# Initialise the likelihood
+likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
+    interferometers=ifos, waveform_generator=waveform_generator,
+    phase_marginalization=True, priors=priors
+)
+
+# Run sampler
+
+# The importance sampling version of nessai is implemented as a different
+# sampler in bilby since the settings are different to standard nessai.
+
+# We can also specify how many posterior samples we want
+
+result = bilby.core.sampler.run_sampler(
+    likelihood=likelihood,
+    priors=priors,
+    outdir=outdir,
+    injection_parameters=injection_parameters,
+    label=label,
+    sampler='nessai_importance',
+    resume=False,
+    plot=True,
+    seed=150914,
+    nlive=5000,
+    n_posterior_samples=5000,
+)
+
+# Produce corner plots
+result.plot_corner()
