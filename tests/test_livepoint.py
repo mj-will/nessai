8ebc1be60eab9103d26f9b1fb8c102cdfaf75b4e
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-06-17 12:22:51 +0100

    Update live point tests for updated defaults

--- tests/test_livepoint.py
+++ tests/test_livepoint.py
@@ -5,6 +5,9 @@ import pytest
 
 from nessai import config
 import nessai.livepoint as lp
+from nessai.utils.testing import (
+    assert_structured_arrays_equal as assert_lp_equal,
+)
 
 
 EXTRA_PARAMS_DTYPE = [
@@ -84,13 +87,28 @@ def test_get_dtype():
     assert dtype == expected
 
 
+def test_empty_structured_array():
+    """Assert the correct default values are used"""
+    n = 10
+    fields = ['x', 'y']
+    array = lp.empty_structured_array(n, fields)
+    for f in fields:
+        np.testing.assert_array_equal(
+            array[f], config.DEFAULT_FLOAT_VALUE * np.ones(n)
+        )
+    for nsp, v in zip(
+        config.NON_SAMPLING_PARAMETERS, config.NON_SAMPLING_DEFAULTS
+    ):
+        np.testing.assert_array_equal(array[nsp], v * np.ones(n))
+
+
 def test_parameters_to_live_point(live_point):
     """
     Test function that produces a single live point given the parameter
     values for the live point as a live or an array
     """
     x = lp.parameters_to_live_point([1., 2., 3.], ['x', 'y', 'z'])
-    np.testing.assert_array_equal(live_point, x)
+    assert_lp_equal(x, live_point)
 
 
 def test_empty_parameters_to_live_point(empty_live_point):
@@ -98,8 +116,9 @@ def test_empty_parameters_to_live_point(empty_live_point):
     Test behaviour when an empty parameter is parsed
     """
     np.testing.assert_array_equal(
+        lp.parameters_to_live_point([], ['x', 'y', 'z']),
         empty_live_point,
-        lp.parameters_to_live_point([], ['x', 'y', 'z']))
+    )
 
 
 def test_numpy_array_to_live_point(live_point):
@@ -109,7 +128,7 @@ def test_numpy_array_to_live_point(live_point):
     """
     array = np.array([1., 2., 3.])
     x = lp.numpy_array_to_live_points(array, names=['x', 'y', 'z'])
-    np.testing.assert_array_equal(live_point, x)
+    assert_lp_equal(live_point, x)
 
 
 def test_numpy_array_multiple_to_live_points(live_points):
@@ -119,7 +138,7 @@ def test_numpy_array_multiple_to_live_points(live_points):
     """
     array = np.array([[1., 2., 3.], [4., 5., 6.]])
     x = lp.numpy_array_to_live_points(array, names=['x', 'y', 'z'])
-    np.testing.assert_array_equal(live_points, x)
+    assert_lp_equal(live_points, x)
 
 
 def test_empty_numpy_array_to_live_points(empty_live_point):
@@ -129,7 +148,8 @@ def test_empty_numpy_array_to_live_points(empty_live_point):
     """
     np.testing.assert_array_equal(
         empty_live_point,
-        lp.numpy_array_to_live_points(np.array([]), names=['x', 'y', 'z']))
+        lp.numpy_array_to_live_points(np.array([]), names=['x', 'y', 'z'])
+    )
 
 
 @pytest.mark.parametrize(
@@ -145,7 +165,7 @@ def test_dict_to_live_point(live_point, d):
     a live point array
     """
     x = lp.dict_to_live_points(d)
-    np.testing.assert_array_equal(live_point, x)
+    assert_lp_equal(live_point, x)
 
 
 @pytest.mark.parametrize(
@@ -161,7 +181,7 @@ def test_dict_multiple_to_live_points(live_points, d):
     a live point array
     """
     x = lp.dict_to_live_points(d)
-    np.testing.assert_array_equal(live_points, x)
+    assert_lp_equal(live_points, x)
 
 
 def test_empty_dict_to_live_points(empty_live_point):
@@ -171,23 +191,25 @@ def test_empty_dict_to_live_points(empty_live_point):
     """
     np.testing.assert_array_equal(
         empty_live_point,
-        lp.dict_to_live_points({'x': [], 'y': [], 'z': []}))
+        lp.dict_to_live_points({'x': [], 'y': [], 'z': []})
+    )
 
 
-def test_dataframe_to_lve_points(live_points):
+def test_dataframe_to_live_points(live_points):
     """Test converting from a pandas dataframe to live points."""
     df = pd.DataFrame({'x': [1, 4], 'y': [2, 5], 'z': [3, 6]})
     out = lp.dataframe_to_live_points(df)
-    np.testing.assert_array_equal(out, live_points)
+    assert_lp_equal(out, live_points)
 
 
 def test_live_point_to_numpy_array(live_point):
     """
     Test conversion from a live point to an unstructured numpy array
     """
-    np.testing.assert_array_equal(
+    (
         np.array([[1, 2, 3, *config.NON_SAMPLING_DEFAULTS]]),
-        lp.live_points_to_array(live_point))
+        lp.live_points_to_array(live_point)
+    )
 
 
 def test_live_point_to_numpy_array_with_names(live_point):
@@ -196,8 +218,9 @@ def test_live_point_to_numpy_array_with_names(live_point):
     specific fields
     """
     np.testing.assert_array_equal(
-        np.array([[1, 3, 0]]),
-        lp.live_points_to_array(live_point, names=['x', 'z', 'logP']))
+        np.array([[1, 3, np.nan]]),
+        lp.live_points_to_array(live_point, names=['x', 'z', 'logP'])
+    )
 
 
 def test_live_point_to_dict(live_point):
@@ -209,15 +232,17 @@ def test_live_point_to_dict(live_point):
         {k: v for k, v in
          zip(config.NON_SAMPLING_PARAMETERS, config.NON_SAMPLING_DEFAULTS)}
     )
-    assert d == lp.live_points_to_dict(live_point)
+    np.testing.assert_equal(lp.live_points_to_dict(live_point), d)
 
 
 def test_live_point_to_dict_with_names(live_point):
     """
     Test conversion of a live point to a dictionary
     """
-    d = {'x': 1., 'z': 3., 'logP': 0.}
-    assert d == lp.live_points_to_dict(live_point, names=['x', 'z', 'logP'])
+    d = {'x': np.array([1.]), 'z': np.array([3.]), 'logP': np.array([np.nan])}
+    np.testing.assert_equal(
+        lp.live_points_to_dict(live_point, names=['x', 'z', 'logP']), d
+    )
 
 
 def test_multiple_live_points_to_dict(live_points):
@@ -226,8 +251,9 @@ def test_multiple_live_points_to_dict(live_points):
     """
     d = {'x': [1, 4], 'y': [2, 5], 'z': [3, 6]}
     d.update({
-        k: 2 * [config.DEFAULT_FLOAT_VALUE]
-        for k in config.NON_SAMPLING_PARAMETERS})
+        k: 2 * [v] for k, v in
+        zip(config.NON_SAMPLING_PARAMETERS, config.NON_SAMPLING_DEFAULTS)
+    })
     d_out = lp.live_points_to_dict(live_points)
     assert list(d.keys()) == list(d_out.keys())
     np.testing.assert_array_equal(list(d.values()), list(d_out.values()))
