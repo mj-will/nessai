7166ce8814a5e8369e6f019a1cd05c07b5f7cda2
Michael J. Williams <michaeljw1@googlemail.com>
2022-06-27 17:28:05 +0100

    test: update test to use MP context

--- tests/test_flowsampler.py
+++ tests/test_flowsampler.py
@@ -396,7 +396,7 @@ def test_safe_exit(flow_sampler):
 @pytest.mark.integration_test
 @pytest.mark.timeout(30)
 @pytest.mark.skip_on_windows
-def test_signal_handling(tmp_path, caplog, model, kwargs):
+def test_signal_handling(tmp_path, caplog, model, kwargs, mp_context):
     """Test the signal handling in nessai.
 
     Test is based on a similar test in bilby which is in turn based on: \
@@ -405,15 +405,16 @@ def test_signal_handling(tmp_path, caplog, model, kwargs):
     output = tmp_path / "output"
     output.mkdir()
 
-    fs = FlowSampler(
-        model,
-        output=output,
-        nlive=500,
-        poolsize=1000,
-        exit_code=5,
-        signal_handling=True,
-        **kwargs
-    )
+    with patch("multiprocessing.Pool", mp_context.Pool):
+        fs = FlowSampler(
+            model,
+            output=output,
+            nlive=500,
+            poolsize=1000,
+            exit_code=5,
+            signal_handling=True,
+            **kwargs
+        )
 
     pid = os.getpid()
 
