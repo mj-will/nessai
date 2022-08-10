65b7b782d572f063653d2e3d340f56608f092599
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-08-10 18:01:10 +0100

    docs: add description

--- nessai/__init__.py
+++ nessai/__init__.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""nessai: Nested sampling with Artificial Intelligence
+
+nessai is a nested sampling algorithm for Bayesian Inference that incorporates
+normalising flows. It is designed for applications where the Bayesian
+likelihood is computationally expensive.
+"""
 import logging
 
 try:
