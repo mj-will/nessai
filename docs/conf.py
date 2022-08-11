78b1985dea1f7f7e2ad6a9c5cf1aa595ec7fd8fb
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-08-11 14:49:04 +0100

    style: apply black code formatter to docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,13 +13,14 @@
 import os
 import sys
 import nessai
-sys.path.insert(0, os.path.abspath('../nessai/'))
+
+sys.path.insert(0, os.path.abspath("../nessai/"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'nessai'
-copyright = '2020, Michael J. Williams'
-author = 'Michael J. Williams'
+project = "nessai"
+copyright = "2020, Michael J. Williams"
+author = "Michael J. Williams"
 
 # The full version, including alpha/beta/rc tags
 release = nessai.__version__
@@ -30,29 +31,30 @@ release = nessai.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax',
-              'numpydoc',
-              'sphinx.ext.autosummary',
-              'sphinx.ext.autosectionlabel',
-              'sphinx.ext.autodoc',
-              'sphinx.ext.inheritance_diagram',
-              'autoapi.extension'
-              ]
+extensions = [
+    "sphinx.ext.mathjax",
+    "numpydoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.inheritance_diagram",
+    "autoapi.extension",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -60,8 +62,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = []
 
 # -- Configure autoapi -------------------------------------------------------
-autoapi_type = 'python'
-autoapi_dirs = ['../nessai/']
+autoapi_type = "python"
+autoapi_dirs = ["../nessai/"]
 autoapi_add_toctree_entry = False
-autoapi_options = ['members', 'show-inheritance',
-                   'show-module-summary']
+autoapi_options = ["members", "show-inheritance", "show-module-summary"]
