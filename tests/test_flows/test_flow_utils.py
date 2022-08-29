3b6ee04b72445c7fea8f6f5588d1f74444d863f5
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-08-29 17:25:13 +0100

    test: move and update MLP tests

--- tests/test_flows/test_flow_utils.py
+++ tests/test_flows/test_flow_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 import torch
 import torch.nn.functional as F
-from unittest.mock import MagicMock, create_autospec, patch
+from unittest.mock import MagicMock, patch
 
 from nessai.flows.utils import (
     configure_model,
@@ -13,7 +13,6 @@ from nessai.flows.utils import (
     silu,
     reset_weights,
     reset_permutations,
-    MLP,
 )
 
 
@@ -81,28 +80,6 @@ def test_weight_reset_permutation():
     y_reset, _ = m(x)
     assert not (p == m._permutation.numpy()).all()
     assert not (y_init.numpy() == y_reset.numpy()).all()
-
-
-def test_mlp_forward():
-    """Test the MLP implementation of forward"""
-    mlp = create_autospec(MLP)
-    x = torch.tensor(1)
-    y = torch.tensor(2)
-    with patch("nflows.nn.nets.MLP.forward", return_value=y) as parent:
-        out = MLP.forward(mlp, x, context=None)
-    parent.assert_called_once_with(x)
-    assert out == y
-
-
-def test_mlp_forward_context():
-    """Assert an error is raised in the context is not None"""
-    mlp = create_autospec(MLP)
-    x = torch.tensor(1)
-    with pytest.raises(NotImplementedError) as excinfo:
-        MLP.forward(mlp, x, context=x)
-    assert "MLP with conditional inputs is not implemented." in str(
-        excinfo.value
-    )
 
 
 def test_configure_model_basic(config):
