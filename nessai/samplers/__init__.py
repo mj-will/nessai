a29fdb371a7099497c48b91d409caa92ce25fcfb
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-10-06 10:00:16 +0100

    docs: add missing doc-string

--- nessai/samplers/__init__.py
+++ nessai/samplers/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""Different nested samplers available in nessai.
+
+All samplers inherit from :py:obj:`nessai.samplers.base.BaseNestedSampler`.
+"""
 from .nestedsampler import NestedSampler
 
 __all__ = [
