9b779b04c3789ecc696d32aba0759098ed059e53
mj-will <m.williams.4@research.gla.ac.uk>
2023-01-17 15:51:44 +0000

    feat: add a separate function for model_config

--- nessai/flowmodel/utils.py
+++ nessai/flowmodel/utils.py
@@ -6,6 +6,41 @@ from .config import DEFAULT_MODEL_CONFIG, DEFAULT_FLOW_CONFIG
 from ..flows.utils import get_n_neurons
 
 
+def update_model_config(d):
+    """Update the model (flow) configuration dictionary based on the defaults.
+
+    Parameters
+    ----------
+    d : Union[dict, None]
+        Dictionary with the current configuration. If None, then the default is
+        used.
+
+    Returns
+    -------
+    dict
+        Updated configuration dictionary.
+
+    Raises
+    ------
+    TypeError
+        Raised if the input is not a dictionary or None.
+    """
+    default = DEFAULT_MODEL_CONFIG.copy()
+    if d is None:
+        return default
+    elif not isinstance(d, dict):
+        raise TypeError(
+            "Must pass a dictionary to update the default model config"
+        )
+    else:
+        default.update(d)
+        default["n_neurons"] = get_n_neurons(
+            n_neurons=default.get("n_neurons"),
+            n_inputs=default.get("n_inputs"),
+        )
+    return default
+
+
 def update_config(d):
     """
     Update the configuration dictionary to include the defaults.
@@ -29,24 +64,18 @@ def update_config(d):
         Dictionary with updated default configuration
     """
     default = DEFAULT_FLOW_CONFIG.copy()
-    default_model = DEFAULT_MODEL_CONFIG.copy()
 
     if d is None:
-        default["model_config"] = default_model
+        default["model_config"] = update_model_config(None)
+    elif not isinstance(d, dict):
+        raise TypeError(
+            "Must pass a dictionary to update the default " "trainer settings"
+        )
     else:
-        if not isinstance(d, dict):
-            raise TypeError(
-                "Must pass a dictionary to update the default "
-                "trainer settings"
-            )
-        else:
-            default.update(d)
-            default_model.update(d.get("model_config", {}))
-            default_model["n_neurons"] = get_n_neurons(
-                n_neurons=default_model.get("n_neurons"),
-                n_inputs=default_model.get("n_inputs"),
-            )
-            default["model_config"] = default_model
+        default.update(d)
+        default["model_config"] = update_model_config(
+            default.get("model_config", None)
+        )
 
     if default["noise_type"] is not None and default["noise_scale"] is None:
         raise RuntimeError(
