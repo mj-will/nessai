b17ef8bf0fd78ef53635e692e96ec6b430d96e81
mj-will <m.williams.4@research.gla.ac.uk>
2023-06-20 12:17:48 +0100

    test: more tests for ins

--- tests/test_samplers/test_importance_nested_sampler/conftest.py
+++ tests/test_samplers/test_importance_nested_sampler/conftest.py
@@ -7,7 +7,9 @@ from nessai.livepoint import (
     add_extra_parameters_to_live_points,
     reset_extra_live_points_parameters,
 )
+from nessai.model import Model
 import numpy as np
+from scipy.special import logsumexp
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -23,7 +25,9 @@ def ins_livepoint_params():
 
 @pytest.fixture
 def ins():
-    return create_autospec(ImportanceNestedSampler)
+    obj = create_autospec(ImportanceNestedSampler)
+    obj.model = create_autospec(Model)
+    return obj
 
 
 @pytest.fixture(scope="module")
@@ -31,18 +35,30 @@ def n_it():
     return 10
 
 
+@pytest.fixture(scope="module")
+def n_samples():
+    return 1000
+
+
 @pytest.fixture
-def samples(model, n_it):
-    x = numpy_array_to_live_points(np.random.randn(1000, 2), model.names)
+def samples(model, n_samples, n_it, log_q):
+    x = numpy_array_to_live_points(
+        np.random.randn(n_samples, len(model.names)), model.names
+    )
     x["it"] = np.random.randint(0, n_it, size=len(x))
     x["logL"] = model.log_likelihood(x)
     x["logP"] = model.log_prior(x)
+
+    alpha = np.unique(x["it"]).astype(float)
+    alpha /= alpha.sum()
+    x["logQ"] = logsumexp(log_q, axis=1, b=alpha)
+    x["logW"] = -x["logQ"].copy()
     return x
 
 
 @pytest.fixture
-def log_q(samples, n_it):
-    return np.random.randn(samples.size, n_it)
+def log_q(n_samples, n_it):
+    return np.random.randn(n_samples, n_it)
 
 
 @pytest.fixture
@@ -67,7 +83,7 @@ def history(n_it):
         "samples_entropy",
         "proposal_entropy",
     ]
-    d = {k: np.random.randn(n_it) for k in keys}
+    d = {k: np.random.randn(n_it).tolist() for k in keys}
     d["stopping_criteria"] = dict(
         dZ=np.arange(n_it),
         ratio=np.arange(n_it),
