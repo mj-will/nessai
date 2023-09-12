5a5111f76e017267478287c15e2d1d76d6b9ccbf
michael.williams <michael.williams@ligo.org>
2023-09-12 01:53:14 -0700

    test: add test for split_kwargs and rename

--- tests/test_utils/test_settings.py
+++ tests/test_utils/test_settings.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Tests for bilbyutils
+Tests for settings utilities.
 """
 from unittest.mock import patch
 
 import pytest
 from nessai.flowsampler import FlowSampler
-from nessai.utils.bilbyutils import (
+from nessai.utils.settings import (
     get_all_kwargs,
     get_run_kwargs_list,
     _get_importance_methods,
@@ -16,14 +16,16 @@ from nessai.utils.bilbyutils import (
 
 def test_get_standard_methods():
     """Assert a list of methods is returned"""
-    out = _get_standard_methods()
-    assert len(out) == 5
+    kwds, run = _get_standard_methods()
+    assert len(kwds) == 4
+    assert len(run) == 1
 
 
 def test_get_importance_methods():
     """Assert a list of methods is returned"""
-    out = _get_importance_methods()
-    assert len(out) == 4
+    kwds, run = _get_importance_methods()
+    assert len(kwds) == 3
+    assert len(run) == 1
 
 
 @pytest.mark.parametrize(
@@ -45,10 +47,37 @@ def test_get_all_kwargs(ins, get_method):
     expected = dict(c=2, d=None, g=3, h=True)
 
     with patch(
-        f"nessai.utils.bilbyutils.{get_method}",
-        return_value=[func0, func1],
+        f"nessai.utils.settings.{get_method}",
+        return_value=[[func0,], [func1,]],
     ):
-        out = get_all_kwargs(importance_nested_sampler=ins)
+        out = get_all_kwargs(importance_nested_sampler=ins, split_kwargs=False)
+
+    assert out == expected
+
+
+@pytest.mark.parametrize(
+    "ins, get_method",
+    [(False, "_get_standard_methods"), (True, "_get_importance_methods")],
+)
+def test_get_all_kwargs_split(ins, get_method):
+    """Assert the correct dictionary is returned.
+
+    Positional arguments should be ignored.
+    """
+
+    def func0(a, b, c=2, d=None):
+        pass
+
+    def func1(e, f, g=3, h=True):
+        pass
+
+    expected = (dict(c=2, d=None), dict(g=3, h=True))
+
+    with patch(
+        f"nessai.utils.settings.{get_method}",
+        return_value=[[func0,], [func1,]],
+    ):
+        out = get_all_kwargs(importance_nested_sampler=ins, split_kwargs=True)
 
     assert out == expected
 
@@ -69,3 +98,4 @@ def test_get_run_kwargs_list(ins, run_method):
         out = get_run_kwargs_list(importance_nested_sampler=ins)
 
     assert out == expected
+
