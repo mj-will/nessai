89b806eae2ae6d77125f9a4559825a9acafc8eb6
mj-will <michaeljw1@googlemail.com>
2024-05-14 15:00:17 +0100

    TST: more tests for glasflow.py

--- tests/test_experimental/test_flows/test_glasflow.py
+++ tests/test_experimental/test_flows/test_glasflow.py
@@ -14,6 +14,18 @@ def test_get_glasflow_class(name):
     FlowClass(n_inputs=2, n_neurons=4, n_blocks=2, n_layers=1)
 
 
+def test_get_glasflow_class_missing_prefix():
+    with pytest.raises(ValueError, match=r"'glasflow' missing from name"):
+        get_glasflow_class("realnvp")
+
+
+def test_get_glasflow_class_invalid_flow():
+    with pytest.raises(
+        ValueError, match=r"invalid is not a known glasflow flow"
+    ):
+        get_glasflow_class("glasflow.invalid")
+
+
 @pytest.mark.integration_test
 def test_glasflow_integration(tmp_path):
 
