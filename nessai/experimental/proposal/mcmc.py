ec4fc4f463afc607648f6cc49f4301fb7edf53fb
mj-will <michaeljw1@googlemail.com>
2024-06-05 08:40:23 +0100

    ENH: add act

--- nessai/experimental/proposal/mcmc.py
+++ nessai/experimental/proposal/mcmc.py
@@ -29,6 +29,66 @@ def _get_nondiagonal_pairs(n: int) -> np.ndarray:
     return pairs
 
 
+def next_pow_two(n):
+    """Returns the next power of two greater than or equal to `n`
+
+    Copied from emcee.
+    """
+    i = 1
+    while i < n:
+        i = i << 1
+    return i
+
+
+def function_1d(x):
+    """Estimate the normalized autocorrelation function of a 1-D series
+
+    Copied from emcee
+    """
+    x = np.atleast_1d(x)
+    if len(x.shape) != 1:
+        raise ValueError("invalid dimensions for 1D autocorrelation function")
+    n = next_pow_two(len(x))
+
+    # Compute the FFT and then (from that) the auto-correlation function
+    f = np.fft.fft(x - np.mean(x), n=2 * n)
+    acf = np.fft.ifft(f * np.conjugate(f))[: len(x)].real
+    if acf[0] == 0.0:
+        acf[:] = np.nan_to_num(np.inf)
+    else:
+        acf /= acf[0]
+    return acf
+
+
+def auto_window(taus, c):
+    m = np.arange(len(taus)) < c * taus
+    if np.any(m):
+        return np.argmin(m)
+    return len(taus) - 1
+
+
+def integrated_time(x, c=5):
+    """Estimate the integrated autocorrelation time of a time series.
+
+    Based on the implementation from emcee.
+    """
+    n_t, n_w, n_d = x.shape
+    tau_est = np.empty(n_d)
+    windows = np.empty(n_d, dtype=int)
+
+    # Loop over parameters
+    for d in range(n_d):
+        f = np.zeros(n_t)
+        for k in range(n_w):
+            f += function_1d(x[:, k, d])
+        f /= n_w
+        taus = 2.0 * np.cumsum(f) - 1.0
+        windows[d] = auto_window(taus, c)
+        tau_est[d] = taus[windows[d]]
+
+    return tau_est
+
+
 def differential_evolution_proposal(x, ensemble, sigma=1e-5):
     pairs = _get_nondiagonal_pairs(ensemble.shape[0])
     indices = np.random.choice(pairs.shape[0], size=x.shape[0], replace=True)
@@ -47,8 +107,9 @@ def gaussian_proposal(x, ensemble, sigma=1e-1):
 class FlowProposalMCMC(FlowProposal):
     """Version of FlowProposal that uses MCMC instead of rejection sampling"""
 
-    def __init__(self, *args, n_steps=50, **kwargs):
+    def __init__(self, *args, n_steps=50, proposal: str = "diff", **kwargs):
         self.n_steps = n_steps
+        self.proposal = proposal
         super().__init__(*args, **kwargs)
 
     def backward_pass(self, z: np.ndarray, rescale: bool = True):
@@ -112,14 +173,28 @@ class FlowProposalMCMC(FlowProposal):
         x_current["logP"] = self.model.batch_evaluate_log_prior(x_current)
         x_current["logL"] = self.model.batch_evaluate_log_likelihood(x_current)
 
-        for _ in range(self.n_steps):
-            z_new = differential_evolution_proposal(
+        z_chain = np.empty((self.n_steps, n_walkers, z_current.shape[-1]))
+        z_chain[0] = z_current
+
+        if self.proposal == "diff":
+            proposal_fn = differential_evolution_proposal
+        elif self.proposal == "gaussian":
+            proposal_fn = gaussian_proposal
+
+        for i in range(self.n_steps):
+            z_new = proposal_fn(
                 z_current,
                 z_ensemble,
             )
+
             x_new, log_j_new = self.backward_pass(z_new, rescale=True)
             x_new["logP"] = self.model.batch_evaluate_log_prior(x_new)
-            x_new["logL"] = self.model.batch_evaluate_log_likelihood(x_new)
+            finite_prior = np.isfinite(x_new["logP"])
+            # Only evaluate function where log-prior is finite
+            # Default is NaN, so will not pass threshold.
+            x_new["logL"][finite_prior] = (
+                self.model.batch_evaluate_log_likelihood(x_new[finite_prior])
+            )
 
             log_factor = (
                 x_new["logP"] + log_j_new - x_current["logP"] - log_j_current
@@ -127,13 +202,18 @@ class FlowProposalMCMC(FlowProposal):
             log_u = np.log(np.random.rand(n_walkers))
             accept = (
                 (log_factor > log_u)
-                & np.isfinite(x_new["logP"])
+                & finite_prior
                 & (x_new["logL"] > log_l_threshold)
             )
 
             x_current[accept] = x_new[accept]
             z_current[accept] = z_new[accept]
             log_j_current[accept] = log_j_new[accept]
+
+            z_chain[i] = z_current
+
+        act = integrated_time(z_chain)
+        logger.info(f"ACT: {act}")
 
         self.samples = self.convert_to_samples(x_current)
 
