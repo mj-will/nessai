689729bdfac9f2063db0ce2cbbca03566a93427c
mj-will <michaeljw1@googlemail.com>
2024-06-27 20:10:59 +0100

    MAINT: improve Gaussian proposal

--- nessai/experimental/proposal/mcmc/steps.py
+++ nessai/experimental/proposal/mcmc/steps.py
@@ -1,10 +1,15 @@
 from functools import lru_cache
+import logging
+import math
 import numpy as np
+
+logger = logging.getLogger(__name__)
 
 
 class Step:
 
-    def __init__(self, ensemble: np.ndarray | None = None) -> None:
+    def __init__(self, dims: int, ensemble: np.ndarray | None = None) -> None:
+        self.dims = dims
         self.update_ensemble(ensemble)
 
     def step(self, z):
@@ -12,6 +17,10 @@ class Step:
 
     def update_ensemble(self, ensemble):
         self.ensemble = ensemble
+
+    def update_stats(self, n_accept, n_reject):
+        self.n_accept = n_accept
+        self.n_reject = n_reject
 
     def __call__(self, *args, **kwargs):
         return self.step(*args, **kwargs)
@@ -21,12 +30,37 @@ class GaussianStep(Step):
 
     def __init__(
         self,
+        dims: int,
         ensemble: np.ndarray | None = None,
+        scale: float | None = None,
+        update_scale: bool = True,
+        target_acceptance: float = 0.5,
     ) -> None:
-        super().__init__(ensemble=ensemble)
+        super().__init__(dims=dims, ensemble=ensemble)
+        if scale is None:
+            self.scale = 2 / self.dims**0.5
+        else:
+            self.scale = scale
+        self.update_scale = update_scale
+        self.target_acceptance = target_acceptance
 
     def step(self, z):
-        return z + self.sigma * np.random.randn(*z.shape), np.zeros(z.shape[0])
+        return np.random.normal(z, scale=self.scale, size=z.shape), np.zeros(
+            z.shape[0]
+        )
+
+    def update_stats(self, n_accept, n_reject):
+        super().update_stats(n_accept, n_reject)
+        if self.update_scale:
+            # This follows the same maths as dynesty
+            acceptance = self.n_accept / (self.n_accept + self.n_reject)
+            logger.debug(f"Current acceptance: {acceptance}")
+            self.scale *= math.exp(
+                (acceptance - self.target_acceptance)
+                / self.dims
+                / self.target_acceptance
+            )
+            logger.debug(f"New scale: {self.scale}")
 
 
 @lru_cache(maxsize=1)
@@ -48,28 +82,38 @@ class DifferentialEvolutionStep(Step):
 
     def __init__(
         self,
+        dims: int,
         ensemble: np.ndarray | None = None,
         mix_fraction: float = 0.5,
         sigma: float = 1e-4,
     ) -> None:
+        self.pairs = None
+        self.n_pairs = None
+
+        super().__init__(dims=dims, ensemble=ensemble)
+
+        self.g0 = 2.38 / np.sqrt(2 * self.dims)
         self.mix_fraction = mix_fraction
         self.sigma = sigma
-        super().__init__(ensemble)
+
+    def update_ensemble(self, ensemble):
+        super().update_ensemble(ensemble)
+        if self.ensemble is not None:
+            self.pairs = _get_nondiagonal_pairs(self.ensemble.shape[0])
+            self.n_pairs = self.pairs.shape[0]
 
     def step(self, z):
-        pairs = _get_nondiagonal_pairs(self.ensemble.shape[0])
-        indices = np.random.choice(
-            pairs.shape[0], size=z.shape[0], replace=True
+        nc = z.shape[0]
+        indices = np.random.choice(self.n_pairs, size=nc, replace=True)
+        diffs = np.diff(self.ensemble[self.pairs[indices]], axis=1).squeeze(
+            axis=1
         )
-        diffs = np.diff(self.ensemble[pairs[indices]], axis=1).squeeze(axis=1)
-        # n = x.shape[0]
-        mix = np.random.rand(z.shape[0]) < self.mix_fraction
-        g0 = 2.38 / np.sqrt(2 * z.shape[1])
-        scale = np.ones((z.shape[0], 1))
-        scale[mix, :] = g0
-        error = self.sigma * np.random.randn(*scale.shape)
+        mix = np.random.rand(nc) < self.mix_fraction
+        scale = np.ones((nc, 1))
+        scale[mix, :] = self.g0
+        error = self.sigma * np.random.randn(nc, self.dims)
         z_new = z + scale * diffs + error
-        return z_new, np.zeros(z.shape[0])
+        return z_new, np.zeros(nc)
 
 
 KNOWN_STEPS = dict(
