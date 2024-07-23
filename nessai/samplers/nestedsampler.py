383a3bb8b35ca5016d9e90e23709ac841bcbdbe7
mj-will <michaeljw1@googlemail.com>
2024-07-23 13:58:40 +0100

    MAINT: tweak return and logging if already finished

--- nessai/samplers/nestedsampler.py
+++ nessai/samplers/nestedsampler.py
@@ -1255,8 +1255,8 @@ class NestedSampler(BaseNestedSampler):
             Array of nested samples.
         """
         if self.finalised:
-            logger.info("Running has already finished!")
-            return self.state.logZ, np.array(self.nested_samples)
+            logger.info("Run has already finished!")
+            return self.log_evidence, np.array(self.nested_samples)
         self.sampling_start_time = datetime.datetime.now()
         if not self.initialised:
             self.initialise(live_points=True)
