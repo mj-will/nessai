0d1bc0d5d4ecdceeb5bf247141317979650e6025
mj-will <michaeljw1@googlemail.com>
2024-08-14 09:47:06 +0100

    EXAMP: fix mcmc example

--- examples/mcmc_example.py
+++ examples/mcmc_example.py
@@ -9,9 +9,6 @@ from nessai.flowsampler import FlowSampler
 from nessai.model import Model
 from nessai.utils import setup_logger
 
-from nessai.experimental.proposal.mcmc import FlowProposalMCMC
-from glasflow.transforms.utils import get_scale_activation
-
 # Setup the logger - credit to the Bilby team for this neat function!
 # see: https://git.ligo.org/lscsoft/bilby
 
@@ -64,65 +61,22 @@ class GaussianModel(Model):
         return log_l
 
 
-class MultimodalModel(Model):
-    """A multimodal gaussian model."""
-
-    def __init__(self, dims=2, multimodal_dims=2, spike_scale=1e-1):
-        self.multimodal_dims = multimodal_dims
-        self.names = [str(i) for i in range(dims)]
-        self.bounds = {i: [-10, 10] for i in self.names}
-        self.vectorised_likelihood = True
-        self.spike_scale = spike_scale
-
-    def log_prior(self, x):
-        """Log-prior"""
-        log_p = np.log(self.in_bounds(x), dtype="float")
-        for bounds in self.bounds.values():
-            log_p -= np.log(bounds[1] - bounds[0])
-        return log_p
-
-    def log_likelihood(self, x):
-        """Log likelihood."""
-        log_l = np.zeros(x.size)
-        for n in self.names[: self.multimodal_dims]:
-            log_l += np.logaddexp(
-                norm(-5, scale=self.spike_scale).logpdf(x[n]),
-                norm(5, scale=self.spike_scale).logpdf(x[n]),
-            )
-        for n in self.names[self.multimodal_dims :]:
-            log_l += norm().logpdf(x[n])
-        return log_l.flatten()
-
-
-from nessai_models import Rosenbrock
-
-
 # The FlowSampler object is used to managed the sampling. Keyword arguments
 # are passed to the nested sampling.
 fs = FlowSampler(
-    Rosenbrock(4),
+    GaussianModel(),
     output=output,
     resume=False,
     seed=1234,
-    flow_class=FlowProposalMCMC,
+    flow_class="mcmcflowproposal",
     n_steps=100,
-    use_approximate_likelihood=False,
-    # check_likelihood=True,
-    # proposal_plots=True,
-    # volume_fraction=0.98,
-    # constant_volume_mode=True,
-    proposal="diff",
     reset_flow=True,
     flow_config=dict(
-        model_config=dict(
-            ftype="glasflow-realnvp",
-            n_neurons=32,
-            n_blocks=6,
-            kwargs=dict(
-                batch_norm_between_transforms=True,
-                scale_activation=get_scale_activation("log2"),
-            ),
-        )
+        ftype="glasflow-realnvp",
+        n_neurons=32,
+        n_blocks=6,
+        batch_norm_between_transforms=True,
+        linear_transform=None,
     ),
 )
 
