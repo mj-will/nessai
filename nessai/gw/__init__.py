3b3fc2c056d5335d98993f0e534a1d7a7e6629f2
mj-will <michaeljw1@googlemail.com>
2024-10-25 17:14:43 +0100

    MAINT: add warning about nessai.gw

--- nessai/gw/__init__.py
+++ nessai/gw/__init__.py
@@ -3,6 +3,18 @@
 Functions and classes for using nessai for gravitational-wave inference.
 """
 
+import warnings
+
 from .proposal import GWFlowProposal
 
 __all__ = ["GWFlowProposal"]
+
+warnings.warn(
+    (
+        "The `nessai.gw` module will be deprecated in the next release in "
+        "favour of the nessai-gw package. This packages provides the same "
+        "functionality as`nessai.gw` via the plugin interface."
+        "For more details, see: https://github.com/mj-will/nessai-gw"
+    ),
+    FutureWarning,
+)
